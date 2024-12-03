e58a2ce95188f72e8bc067f5e105f710ba708203
Polaris <clepolaris7@gmail.com>
2024-12-03 19:55:30 +0800

    -

---
+++ DimDungeon/DimDungeonExtension.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Diagnostics.Contracts;
+using Microsoft.Xna.Framework;
+
+namespace CalamityEntropy.DimDungeon;
+
+public static class DimDungeonExtension
+{
+    [Pure]
+    public static bool IsHorizontal(this Direction direction)
+    {
+        return direction == Direction.Left || direction == Direction.Right;
+    }
+
+    [Pure]
+    public static Direction Reverse(this Direction direction)
+    {
+        switch (direction)
+        {
+            case Direction.Up:
+                return Direction.Down;
+            case Direction.Down:
+                return Direction.Up;
+            case Direction.Left:
+                return Direction.Right;
+            case Direction.Right:
+                return Direction.Left;
+        }
+        throw new ArgumentException("Invalid direction");
+    }
+}
---
+++ DimDungeon/DimDungeonPlayer.cs
@@ -0,0 +1,16 @@
+﻿using CalamityEntropy.Util;
+using SubworldLibrary;
+using Terraria.ModLoader;
+
+namespace CalamityEntropy.DimDungeon;
+
+public class DimDungeonPlayer: ModPlayer
+{
+    public override void PreUpdate()
+    {
+        if (SubworldSystem.IsActive<DimDungeonSubworld>())
+        {
+            Player.Entropy().crSky = 5;
+        }
+    }
+}
