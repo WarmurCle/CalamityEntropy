792357ba0fce8332d11f5e95eef76b0fd06b75eb
Materialistic989286067 <3608728857@qq.com>
2025-03-31 21:58:31 +0800

    Update YharimPower.cs

--- Content/Buffs/YharimPower.cs
+++ Content/Buffs/YharimPower.cs
@@ -1,34 +1,30 @@
-using CalamityMod;
-using Terraria;
-using Terraria.ID;
-using Terraria.ModLoader;
+使用 CalamityMod;
+使用泰拉瑞亚;
+使用 Terraria.ID;
+使用 Terraria.ModLoader;
 
-namespace CalamityEntropy.Content.Buffs
+命名空间 CalamityEntropy.内容.增益
 {
-    public class YharimPower : ModBuff
+    公共 类 YharimPower : ModBuff
     {
-        public override void SetStaticDefaults()
+        公共 覆盖 无返回值 SetStaticDefaults()
         {
-            Main.debuff[Type] = false;
-            Main.pvpBuff[Type] = true;
-            Main.buffNoSave[Type] = false;
-            BuffID.Sets.LongerExpertDebuff[Type] = false;
+            Main.debuff[Type] = 假;
+            Main.pvpBuff[Type] = 真;
+            Main.buffNoSave[Type] =   假;
+            BuffID.Sets LongExpertDebuff[Type] = 假;
         }
 
-        public override void Update(Player player, ref int buffIndex)
+        公共 覆盖 无返回值 更新(玩家 player, 引用 整数 buffIndex)
         {
             player.GetDamage(DamageClass.Generic) += 0.06f;
             player.GetCritChance(DamageClass.Generic) += 5;
             player.pickSpeed -= 0.3f;
             player.GetAttackSpeed(DamageClass.Melee) *= 1.1f;
             player.GetKnockback(DamageClass.Summon) *= 2;
-            player.moveSpeed *= 1.2f;
-            player.luck += 4;
-            player.GetArmorPenetration(DamageClass.Generic) += 10f;
-            player.Calamity().contactDamageReduction *= 1.08f;
-            player.Calamity().projectileDamageReduction *= 1.08f;
+            player.moveSpeed *= 1.1f;
             player.statDefense += 10;
-            player.lifeRegen += 8;
+            player.lifeRegen += 6;
         }
     }
-}
+输入：}
