7060e54e75e334f0564959e124e651334ee99c43
Materialistic989286067 <3608728857@qq.com>
2025-04-01 19:53:36 +0800

    Update Revelation.cs

--- Content/Items/Weapons/Revelation.cs
+++ Content/Items/Weapons/Revelation.cs
@@ -17,10 +17,10 @@ namespace CalamityEntropy.Content.Items.Weapons
         {
             Item.width = 36;
             Item.height = 34;
-            Item.damage = 110;
+            Item.damage = 210;
             Item.noMelee = true;
             Item.noUseGraphic = true;
-            Item.useAnimation = Item.useTime = 20;
+            Item.useAnimation = Item.useTime = 16;
             Item.useStyle = ItemUseStyleID.Swing;
             Item.knockBack = 1f;
             Item.UseSound = SoundID.Item1;
@@ -29,6 +29,7 @@ namespace CalamityEntropy.Content.Items.Weapons
             Item.value = CalamityGlobalItem.RarityOrangeBuyPrice / 4; Item.rare = ItemRarityID.Orange;
             Item.shoot = ModContent.ProjectileType<RevelationThrow>();
             Item.shootSpeed = 45f;
+            Item.ArmorPenetration = 30;
             Item.DamageType = CUtil.rogueDC;
             Item.rare = ItemRarityID.Red;
             Item.Entropy().tooltipStyle = 3;
@@ -64,10 +65,11 @@ namespace CalamityEntropy.Content.Items.Weapons
         public override void AddRecipes()
         {
             Recipe recipe = CreateRecipe();
+            recipe.AddIngredient(ModContent.ItemType<TwistingNether>(), 3);
             recipe.AddIngredient(ModContent.ItemType<MeldConstruct>(), 5);
             recipe.AddIngredient(ModContent.ItemType<ShardofAntumbra>(), 1);
             recipe.AddIngredient(ItemID.DeathSickle, 1);
-            recipe.AddTile(TileID.LunarCraftingStation);
+	        recipe.AddTile(TileID.LunarCraftingStation);
             recipe.Register();
         }
     }
