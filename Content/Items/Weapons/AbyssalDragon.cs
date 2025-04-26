310bea12cb43a5c1d0e3e531f92f3393b5e70214
Polaris <2058770793@qq.com>
2025-04-26 16:39:39 +0800

    --

--- Content/Items/Weapons/AbyssalDragon.cs
+++ Content/Items/Weapons/AbyssalDragon.cs
@@ -24,7 +24,7 @@ namespace CalamityEntropy.Content.Items.Weapons
         {
             Item.width = 84;
             Item.height = 84;
-            Item.damage = 630;
+            Item.damage = 760;
             Item.noMelee = true;
             Item.crit = 10;
             Item.useAnimation = Item.useTime = 32;
