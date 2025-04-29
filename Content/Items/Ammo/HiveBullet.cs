1aa27ad521aa426f2dd0351136820c95f883f8ff
Materialistic989286067 <3608728857@qq.com>
2025-04-29 14:09:00 +0800

    Update HiveBullet.cs

--- Content/Items/Ammo/HiveBullet.cs
+++ Content/Items/Ammo/HiveBullet.cs
@@ -17,7 +17,7 @@ namespace CalamityEntropy.Content.Items.Ammo
 
         public override void SetDefaults()
         {
-            Item.damage = 6;
+            Item.damage = 1;
             Item.DamageType = DamageClass.Ranged;
             Item.width = 8;
             Item.height = 8;
