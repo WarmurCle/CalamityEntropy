8e67fbfc53cf001679649970680324901e10707a
Materialistic989286067 <3608728857@qq.com>
2025-05-14 21:42:48 +0800

    Update TheBeginingAndTheEnd.cs

--- Content/Items/Weapons/TheBeginingAndTheEnd.cs
+++ Content/Items/Weapons/TheBeginingAndTheEnd.cs
@@ -22,11 +22,11 @@ namespace CalamityEntropy.Content.Items.Weapons
         {
             Item.width = 50;
             Item.height = 38;
-            Item.damage = 2400;
+            Item.damage = 2800;
             Item.ArmorPenetration = 80;
             Item.noMelee = true;
             Item.noUseGraphic = true;
-            Item.useAnimation = Item.useTime = 20;
+            Item.useAnimation = Item.useTime = 15;
             Item.useStyle = ItemUseStyleID.Swing;
             Item.ArmorPenetration = 86;
             Item.knockBack = 1f;
