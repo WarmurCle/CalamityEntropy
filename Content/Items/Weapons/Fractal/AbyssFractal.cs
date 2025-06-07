70edd47bde380a70469cda63c5f9382fe6b1339d
Materialistic989286067 <3608728857@qq.com>
2025-06-07 11:54:52 +0800

    Update AbyssFractal.cs

--- Content/Items/Weapons/Fractal/AbyssFractal.cs
+++ Content/Items/Weapons/Fractal/AbyssFractal.cs
@@ -36,6 +36,7 @@ namespace CalamityEntropy.Content.Items.Weapons.Fractal
             Item.autoReuse = true;
             Item.shoot = ModContent.ProjectileType<AbyssFractalHeld>();
             Item.shootSpeed = 12f;
+            Item.ArmorPenetration = 30;
         }
         public int atkType = 1;
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
