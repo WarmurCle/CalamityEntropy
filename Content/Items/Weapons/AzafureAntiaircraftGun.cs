d73a7dd44e14d33f7b88d787973676e7ac41d6e3
Polaris <2058770793@qq.com>
2025-06-16 17:20:50 +0800

    -

--- Content/Items/Weapons/AzafureAntiaircraftGun.cs
+++ Content/Items/Weapons/AzafureAntiaircraftGun.cs
@@ -1,4 +1,5 @@
-﻿using CalamityEntropy.Content.Projectiles;
+﻿using CalamityEntropy.Content.Particles;
+using CalamityEntropy.Content.Projectiles;
 using CalamityMod;
 using CalamityMod.Items;
 using CalamityMod.Items.Materials;
@@ -15,13 +16,13 @@ namespace CalamityEntropy.Content.Items.Weapons
 {
     public class AzafureAntiaircraftGun : ModItem
     {
-        public override bool IsLoadingEnabled(Mod mod)
+        public override void SetStaticDefaults()
         {
-            return false;
+            AmmoID.Sets.SpecificLauncherAmmoProjectileFallback[Type] = ItemID.RocketLauncher;
         }
         public override void SetDefaults()
         {
-            Item.damage = 535;
+            Item.damage = 2300;
             Item.crit = 10;
             Item.DamageType = DamageClass.Ranged;
             Item.width = 194;
@@ -30,27 +31,27 @@ namespace CalamityEntropy.Content.Items.Weapons
             Item.useAnimation = 90;
             Item.useStyle = ItemUseStyleID.Shoot;
             Item.knockBack = 16;
-            Item.value = CalamityGlobalItem.RarityOrangeBuyPrice;
+            Item.value = CalamityGlobalItem.RarityRedBuyPrice;
             Item.rare = ModContent.RarityType<DarkOrange>();
             Item.UseSound = null;
             Item.noMelee = true;
             Item.noUseGraphic = true;
             Item.shoot = ModContent.ProjectileType<AzAAGunHoldout>();
             Item.shootSpeed = 12;
+            Item.useAmmo = AmmoID.Rocket;
         }
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
-            Projectile.NewProjectile(source, position, velocity, type, damage, knockback, player.whoAmI);
+            Projectile.NewProjectile(source, position, velocity, Item.shoot, damage, knockback, player.whoAmI, type);
             return false;
         }
         public override void AddRecipes()
         {
-            CreateRecipe().AddIngredient<AerialiteBar>(5)
-                .AddIngredient<DubiousPlating>(15)
-                .AddIngredient<EnergyCore>(2)
+            CreateRecipe().AddIngredient<ScoriaBar>(6)
+                .AddIngredient<DubiousPlating>(10)
                 .AddIngredient(ItemID.HellstoneBar, 18)
-                .AddIngredient(ItemID.IronBar, 20)
-                .AddTile(TileID.Anvils)
+                .AddIngredient(ItemID.MeteoriteBar, 6)
+                .AddTile(TileID.MythrilAnvil)
                 .Register();
         }
     }
@@ -70,6 +71,7 @@ namespace CalamityEntropy.Content.Items.Weapons
         public float BarrelOffset = 0;
         public bool shoot = true;
         public bool steamSound = true;
+        public bool slSound = true;
         public override void AI()
         {
             Player player = Projectile.GetOwner();
@@ -84,11 +86,20 @@ namespace CalamityEntropy.Content.Items.Weapons
             {
                 BarrelOffset = 0;
             }
+            if(progress > 0.6f && slSound)
+            {
+                slSound = false;
+                CEUtils.PlaySound("shellLand", 1, Projectile.Center);
+            }
             if(progress > 0.2f)
             {
                 if (steamSound)
                 {
                     steamSound = false;
+                    if(Main.myPlayer == Projectile.owner)
+                    {
+                        Projectile.NewProjectile(Projectile.GetSource_FromAI(), Projectile.Center - Projectile.velocity.normalize() * 120, Projectile.velocity.RotatedBy(-2).normalize() * 16, ModContent.ProjectileType<AntiaircraftShell>(), 0, 0, Projectile.owner);
+                    }
                     for(int i = 0; i < 14; i++)
                     {
                         Color smokeColor = CalamityUtils.MulticolorLerp(Main.rand.NextFloat(), new Color[3] {Color.White, Color.Gray, Color.LightGray});
@@ -102,10 +113,12 @@ namespace CalamityEntropy.Content.Items.Weapons
             }
             if(shoot && Main.myPlayer == Projectile.owner)
             {
-                Projectile.NewProjectile(Projectile.GetSource_FromAI(), Projectile.Center + Projectile.rotation.ToRotationVector2() * 130, Projectile.rotation.ToRotationVector2() * 42, ProjectileID.EnchantedBeam, Projectile.damage, Projectile.knockBack, Projectile.owner);
+                Projectile.NewProjectile(Projectile.GetSource_FromAI(), Projectile.Center + Projectile.rotation.ToRotationVector2() * 130, Projectile.rotation.ToRotationVector2() * 42, ModContent.ProjectileType<AzAGShot>(), Projectile.damage, Projectile.knockBack, Projectile.owner, Projectile.ai[0]);
             }
             if (shoot)
             {
+                EParticle.NewParticle(new Particles.ImpactParticle(), Projectile.Center + Projectile.velocity.normalize() * 150, Vector2.Zero, Color.LightGoldenrodYellow, 0.12f, 1, true, BlendState.Additive, Projectile.rotation);
+
                 CEUtils.PlaySound("AAGShot", 1, Projectile.Center);
                 CEUtils.SetShake(Projectile.Center, 10);
                 for (int i = 0; i < 16; i++)
@@ -152,6 +165,59 @@ namespace CalamityEntropy.Content.Items.Weapons
             Main.EntitySpriteDraw(tex, Projectile.Center - Main.screenPosition, null, lightColor, Projectile.rotation, tex.Size() / 2f, Projectile.scale, Projectile.velocity.X > 0 ? SpriteEffects.None : SpriteEffects.FlipVertically);
             
             return false;
+        }
+    }
+    public class AzAGShot : ModProjectile
+    {
+
+        public override void SetDefaults()
+        {
+            Projectile.width = 32;
+            Projectile.height = 32;
+            Projectile.friendly = true;
+            Projectile.DamageType = DamageClass.Ranged;
+            Projectile.timeLeft = 120;
+            Projectile.tileCollide = false;
+            Projectile.usesIDStaticNPCImmunity = true;
+            Projectile.idStaticNPCHitCooldown = 10;
+            Projectile.penetrate = -1;
+            Projectile.MaxUpdates = 4;
+            Projectile.light = 0.4f;
+            Projectile.scale = 2;
+        }
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            CEUtils.PlaySound("ystn_hit", 0.8f, Projectile.Center);
+            for (int i = 0; i < 16; i++)
+            {
+                Vector2 top = target.Center;
+                Vector2 sparkVelocity2 = Projectile.rotation.ToRotationVector2().RotateRandom(0.3f) * Main.rand.NextFloat(16f, 36f);
+                int sparkLifetime2 = Main.rand.Next(16, 26);
+                float sparkScale2 = Main.rand.NextFloat(1f, 1.8f);
+                var sparkColor2 = Color.Lerp(Color.Goldenrod, Color.Yellow, Main.rand.NextFloat(0, 1));
+
+                LineParticle spark = new LineParticle(top, sparkVelocity2, false, (int)(sparkLifetime2), sparkScale2, sparkColor2);
+                GeneralParticleHandler.SpawnParticle(spark);
+            }
+            var p = new Projectile();
+            p.SetDefaults((int)Projectile.ai[0]);
+            p.whoAmI = 0;
+            ProjectileLoader.OnHitNPC(p, target, hit, damageDone);
+        }
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Main.EntitySpriteDraw(Projectile.getDrawData(lightColor));
+            return false;
+        }
+
+        public override void AI()
+        {
+            Projectile.rotation = Projectile.velocity.ToRotation();
+            for(float i = 0; i < 1; i+= 0.2f)
+            {
+                EParticle.spawnNew(new Smoke() { timeleftmax = 16, Lifetime = 16 }, Projectile.Center + CEUtils.randomPointInCircle(4) - Projectile.velocity * i, Projectile.velocity * 0.6f + CEUtils.randomPointInCircle(0.4f), Color.OrangeRed, Main.rand.NextFloat(0.04f, 0.06f), 1, true, BlendState.Additive, CEUtils.randomRot());
+            }
+            EParticle.spawnNew(new EMediumSmoke(), Projectile.Center, CEUtils.randomPointInCircle(4), Color.LightGoldenrodYellow, Main.rand.NextFloat(0.4f, 0.8f), 1, true, BlendState.AlphaBlend, CEUtils.randomRot());
         }
     }
 }
