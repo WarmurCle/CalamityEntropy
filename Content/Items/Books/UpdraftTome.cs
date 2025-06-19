9611b6ae1cad783947db70a606d40a3b47228dd3
Materialistic989286067 <3608728857@qq.com>
2025-06-19 20:23:17 +0800

    Update UpdraftTome.cs

--- Content/Items/Books/UpdraftTome.cs
+++ Content/Items/Books/UpdraftTome.cs
@@ -12,7 +12,7 @@ namespace CalamityEntropy.Content.Items.Books
         public override void SetDefaults()
         {
             base.SetDefaults();
-            Item.damage = 58;
+            Item.damage = 24;
             Item.useAnimation = Item.useTime = 20;
             Item.crit = 5;
             Item.mana = 6;
