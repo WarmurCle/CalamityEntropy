2947b30bd60da0042e2c21b1eb94d9196d316835
Materialistic989286067 <3608728857@qq.com>
2025-06-21 12:54:25 +0800

    Update HadopelagicEchoII.cs

--- Content/Items/Weapons/HadopelagicEchoII.cs
+++ Content/Items/Weapons/HadopelagicEchoII.cs
@@ -46,6 +46,7 @@ namespace CalamityEntropy.Content.Items.Weapons
         {
             CreateRecipe().AddIngredient(ModContent.ItemType<EidolicWail>())
                 .AddIngredient(ModContent.ItemType<WyrmTooth>(), 12)
+                .AddIngredient(ModContent.ItemType<ShadowspecBar>(), 5)
                 .AddTile(ModContent.TileType<AbyssalAltarTile>())
                 .Register();
         }
