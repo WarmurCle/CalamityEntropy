549ad2e076b8e59257c577497f6b1185ca6ea63b
Materialistic989286067 <3608728857@qq.com>
2025-06-21 14:42:13 +0800

    Update Xytheron.cs

--- Content/Items/Weapons/Xytheron.cs
+++ Content/Items/Weapons/Xytheron.cs
@@ -2,6 +2,7 @@
 using CalamityEntropy.Content.Rarities;
 using CalamityEntropy.Content.Tiles;
 using CalamityMod;
+using CalamityMod.Items.Materials;
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.ID;
