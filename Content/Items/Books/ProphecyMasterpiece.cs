afa56cdb739409ab26c140ef3e5de172e56e2ca0
Materialistic989286067 <3608728857@qq.com>
2025-06-28 10:05:35 +0800

    Update ProphecyMasterpiece.cs

--- Content/Items/Books/ProphecyMasterpiece.cs
+++ Content/Items/Books/ProphecyMasterpiece.cs
@@ -18,7 +18,7 @@ namespace CalamityEntropy.Content.Items.Books
         public override void SetDefaults()
         {
             base.SetDefaults();
-            Item.damage = 42;
+            Item.damage = 58;
             Item.useAnimation = Item.useTime = 16;
             Item.crit = 10;
             Item.mana = 8;
