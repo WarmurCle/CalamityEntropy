612e3db61a849d9219b158e9de48e7ccc1fa2753
Polaris <2058770793@qq.com>
2025-07-01 19:12:44 +0800

    -

--- Common/EGlobalNPC.cs
+++ Common/EGlobalNPC.cs
@@ -1137,7 +1137,7 @@ namespace CalamityEntropy.Common
             {
                 if (player.Entropy().grudgeCard)
                 {
-                    if (Main.rand.NextBool(10))
+                    if (Main.rand.NextBool(24))
                     {
                         Projectile.NewProjectile(player.GetSource_FromThis(), npc.Center, CEUtils.randomPointInCircle(10), ModContent.ProjectileType<HealingSpirit>(), 0, 0, player.whoAmI);
                     }
