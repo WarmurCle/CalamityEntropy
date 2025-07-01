72fd8c76ed714c8920ce2aca4290c4dc99ab7264
Polaris <2058770793@qq.com>
2025-07-01 20:59:37 +0800

    Donor Item

---
+++ Content/Items/Donator/IDonatorItem.cs
@@ -0,0 +1,34 @@
+﻿using CalamityMod;
+using CalamityMod.Items;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.DataStructures;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityEntropy.Content.Items.Donator
+{
+    public interface IDonatorItem
+    {
+        public string DonatorName { get; }
+    }
+    public class DonatorGItem : GlobalItem
+    {
+        public override void SetDefaults(Item entity)
+        {
+            if (entity.ModItem != null && entity.ModItem is IDonatorItem i)
+            {
+                entity.Calamity().donorItem = true;
+            }
+        }
+
+        public override void ModifyTooltips(Item entity, List<TooltipLine> tooltips)
+        {
+            if (entity.ModItem != null && entity.ModItem is IDonatorItem i) 
+            {
+                TooltipLine tl = new TooltipLine(Mod, "CalamityEntropy:Donor", Mod.GetLocalization("Donor").Value + " " + i.DonatorName);
+                tooltips.Add(tl);
+            }
+        }
+    }
+}
