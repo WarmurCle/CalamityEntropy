d9049c0ceea6c63b150d4c12c4a79a5a4266aa64
Polaris <2058770793@qq.com>
2025-07-02 10:39:31 +0800

    fix

--- Content/Items/Books/BookMarks/BookMarkAbyss.cs
+++ Content/Items/Books/BookMarks/BookMarkAbyss.cs
@@ -32,7 +32,7 @@ namespace CalamityEntropy.Content.Items.Books.BookMarks
             {
                 int damage = projectile.damage / 15;
                 Vector2 p = target.Center + CEUtils.randomRot().ToRotationVector2() * 300;
-                Projectile.NewProjectile(projectile.GetSource_FromThis(), p, (target.Center - p).SafeNormalize(Vector2.One), ModContent.ProjectileType<AbyssBookmarkCrack>(250), (int)projectile.GetOwner().GetTotalDamage(projectile.DamageType).ApplyTo(300), projectile.knockBack, projectile.owner);
+                Projectile.NewProjectile(projectile.GetSource_FromThis(), p, (target.Center - p).SafeNormalize(Vector2.One), ModContent.ProjectileType<AbyssBookmarkCrack>(), (int)projectile.GetOwner().GetTotalDamage(projectile.DamageType).ApplyTo(250), projectile.knockBack, projectile.owner);
                 Main.LocalPlayer.Calamity().GeneralScreenShakePower = 5;
                 CEUtils.PlaySound("crack", 1, projectile.Center, 3);
             }
