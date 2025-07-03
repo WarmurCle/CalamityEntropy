1a60c98a78edfc0db7fed41eb78a976cd10740be
Materialistic989286067 <3608728857@qq.com>
2025-07-03 23:22:59 +0800

    Update ControlTerminal.cs

--- Content/Items/Books/ControlTerminal.cs
+++ Content/Items/Books/ControlTerminal.cs
@@ -27,7 +27,7 @@ namespace CalamityEntropy.Content.Items.Books
         public override void SetDefaults()
         {
             base.SetDefaults();
-            Item.damage = 110;
+            Item.damage = 90;
             Item.useAnimation = Item.useTime = 100;
             Item.crit = 10;
             Item.mana = 36;
