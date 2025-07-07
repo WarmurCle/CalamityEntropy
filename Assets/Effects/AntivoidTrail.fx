04cd914eae871fe9854b448468e8982345eab91f
Polaris <2058770793@qq.com>
2025-07-07 11:49:16 +0800

    avd

---
+++ Assets/Effects/AntivoidTrail.fx
@@ -0,0 +1,25 @@
+sampler uImage : register(s0);
+sampler uTrail : register(s1);
+sampler pLight : register(s2);
+float alpha;
+float offset;
+float4 EffectFunction(float2 coords : TEXCOORD0) : COLOR0
+{
+    float4 color1 = tex2D(uImage, float2(offset + coords.x, coords.y));
+    float4 color2 = tex2D(uTrail, coords);
+    float4 color3 = tex2D(pLight, coords);
+    float z = 0;
+    if(color2.r > 0.5)
+    {
+        z = (color2.r - 0.5) * 5;
+    }
+    return ((color2 * float4(0.2, 0.06, 0.3, color2.r + z)) + (color1 * float4(1, 0.24, 0.586, color2.r + z)) * color2.r + color3 * float4(0.3, 0.2, 0.8, 0.2 * color3.r * (color2.r + z))) * float4(1, 1, 1, alpha);
+}
+
+technique Technique1
+{
+    pass EffectPass
+    {
+        PixelShader = compile ps_2_0 EffectFunction();
+    }
+}
--- Content/Items/Weapons/AntiVoid.cs
+++ Content/Items/Weapons/AntiVoid.cs
@@ -2,6 +2,11 @@
 using CalamityEntropy.Content.Rarities;
 using CalamityMod;
 using CalamityMod.Items;
+using CalamityMod.Particles;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using System;
+using System.Collections.Generic;
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.ID;
@@ -24,19 +29,18 @@ namespace CalamityEntropy.Content.Items.Weapons
             Item.noMelee = true;
             Item.noUseGraphic = true;
             Item.useStyle = ItemUseStyleID.Shoot;
-            Item.useTime = 30;
-            Item.useAnimation = 30;
+            Item.useTime = 8;
+            Item.useAnimation = 16;
             Item.autoReuse = true;
             Item.scale = 2f;
             Item.DamageType = DamageClass.MeleeNoSpeed;
             Item.damage = 100;
             Item.knockBack = 4;
             Item.crit = 6;
-            Item.shoot = ModContent.ProjectileType<VoidshadeHeld>();
+            Item.shoot = ModContent.ProjectileType<AntivoidSlash>();
             Item.shootSpeed = 16;
             Item.value = CalamityGlobalItem.RarityPinkBuyPrice;
             Item.rare = ModContent.RarityType<VoidPurple>();
-            Item.Calamity().devItem = true;
         }
         public override bool AltFunctionUse(Player player)
         {
@@ -44,12 +48,7 @@ namespace CalamityEntropy.Content.Items.Weapons
         }
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
         {
-            if (player.altFunctionUse == 2)
-            {
-            }
-            else
-            {
-            }
+            Projectile.NewProjectile(source, position, CEUtils.randomRot().ToRotationVector2() * velocity.Length(), type, damage, knockback, player.whoAmI, Main.rand.NextBool() ? -1 : 1);
             return false;
         }
 
@@ -61,6 +60,193 @@ namespace CalamityEntropy.Content.Items.Weapons
 
         public override void AddRecipes()
         {
+        }
+    }
+
+    public class AntivoidSlash : ModProjectile
+    {
+        List<float> odr = new List<float>();
+        List<float> odl = new List<float>();
+        public override void SetStaticDefaults()
+        {
+            Main.projFrames[Projectile.type] = 1;
+            ProjectileID.Sets.TrailingMode[Type] = 2;
+            ProjectileID.Sets.TrailCacheLength[Type] = 12;
+
+        }
+        public override void SetDefaults()
+        {
+            Projectile.DamageType = ModContent.GetInstance<TrueMeleeDamageClass>();
+            Projectile.width = 1;
+            Projectile.height = 1;
+            Projectile.friendly = true;
+            Projectile.penetrate = -1;
+            Projectile.tileCollide = false;
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = -1;
+            Projectile.timeLeft = 100000;
+            Projectile.MaxUpdates = 12;
+        }
+        public float counter = 0;
+        public float scale = 1;
+        public float alpha = 0;
+        public bool init = true;
+        public bool shoot = true;
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            CEUtils.SetShake(target.Center, 6, 3000);
+            CEUtils.PlaySound("antivoidhit", Main.rand.NextFloat(0.8f, 1.2f), target.Center);
+            Color impactColor = Color.LightBlue;
+            float impactParticleScale = Main.rand.NextFloat(1.4f, 1.6f);
+
+            SparkleParticle impactParticle = new SparkleParticle(target.Center + Main.rand.NextVector2Circular(target.width * 0.75f, target.height * 0.75f), Vector2.Zero, impactColor, Color.Blue, impactParticleScale, 8, 0, 2.5f);
+            GeneralParticleHandler.SpawnParticle(impactParticle);
+
+
+            float sparkCount = 32;
+            for (int i = 0; i < sparkCount; i++)
+            {
+                float p = Main.rand.NextFloat();
+                Vector2 sparkVelocity2 = (target.Center - Projectile.Center).normalize().RotatedByRandom(p * 0.4f) * Main.rand.NextFloat(6, 34 * (2 - p));
+                int sparkLifetime2 = (int)((2 - p) * 7);
+                float sparkScale2 = 0.6f + (1 - p);
+                Color sparkColor2 = Color.Lerp(Color.DeepSkyBlue, Color.Purple, p);
+                if (Main.rand.NextBool())
+                {
+                    AltSparkParticle spark = new AltSparkParticle(target.Center + Main.rand.NextVector2Circular(target.width * 0.5f, target.height * 0.5f), sparkVelocity2 * (1f), false, (int)(sparkLifetime2 * (1.2f)), sparkScale2 * (1.4f), sparkColor2);
+                    GeneralParticleHandler.SpawnParticle(spark);
+                }
+                else
+                {
+                    LineParticle spark = new LineParticle(target.Center + Main.rand.NextVector2Circular(target.width * 0.5f, target.height * 0.5f), sparkVelocity2 * (Projectile.frame == 7 ? 1f : 0.65f), false, (int)(sparkLifetime2 * (Projectile.frame == 7 ? 1.2f : 1f)), sparkScale2 * (Projectile.frame == 7 ? 1.4f : 1f), Main.rand.NextBool() ? Color.Red : Color.Firebrick);
+                    GeneralParticleHandler.SpawnParticle(spark);
+                }
+            }
+        }
+        public float RotF = 0;
+        public float rc = 0;
+        public float yc = 0;
+        public override void AI()
+        {
+            if(yc == 0)
+            {
+                yc = Main.rand.NextFloat(0.5f, 0.8f);
+            }
+            Player owner = Projectile.GetOwner();
+            float MaxUpdateTimes = 14 * Projectile.MaxUpdates;
+            float progress = (counter / MaxUpdateTimes);
+            counter++;
+            if (init)
+            {
+                CEUtils.PlaySound("antivoiduse", Main.rand.NextFloat(0.7f, 1.4f), Projectile.Center);
+                Projectile.scale *= owner.HeldItem.scale;
+                init = false;
+            }
+            Projectile.timeLeft = 3;
+            if(RotF == 0)
+            {
+                RotF = Main.rand.NextFloat(MathHelper.ToRadians(340), MathHelper.ToRadians(400));
+                rc = Main.rand.NextFloat(0.2f, 0.4f);
+            }
+            alpha = 1;
+            scale = 1f;
+            float cr = MathHelper.ToRadians(140);
+            Vector2 jw = ((RotF * -rc + CEUtils.Parabola(progress * 0.5f, RotF))).ToRotationVector2() * new Vector2(1, yc);
+            lg = (jw).Length();
+
+            Projectile.rotation = Projectile.velocity.ToRotation() + jw.ToRotation() * Projectile.ai[0];
+
+            Projectile.Center = Projectile.GetOwner().MountedCenter;
+
+            if (Projectile.velocity.X > 0)
+            {
+                owner.direction = 1;
+                owner.SetCompositeArmFront(true, Player.CompositeArmStretchAmount.Full, Projectile.rotation - (float)(Math.PI * 0.5f));
+            }
+            else
+            {
+                owner.direction = -1;
+                owner.SetCompositeArmFront(true, Player.CompositeArmStretchAmount.Full, Projectile.rotation - (float)(Math.PI * 0.5f));
+            }
+            if (counter > MaxUpdateTimes)
+            {
+                Projectile.Kill();
+            }
+            odr.Add(Projectile.rotation);
+            odl.Add(lg);
+            if (odr.Count > 110)
+            {
+                odl.RemoveAt(0);
+                odr.RemoveAt(0);
+            }
+        }
+        public float lg = 0;
+        public override void DrawBehind(int index, List<int> behindNPCsAndTiles, List<int> behindNPCs, List<int> behindProjectiles, List<int> overPlayers, List<int> overWiresUI)
+        {
+            overPlayers.Add(index);
+        }
+        public override bool ShouldUpdatePosition()
+        {
+            return false;
+        }
+        public Texture2D tex => Projectile.GetTexture();
+        public float rofs = 0f;
+        public override bool PreDraw(ref Color lightColor)
+        {
+            rofs += 0.01f;
+            Texture2D trail = CEUtils.getExtraTex("RuneRibbon");
+            List<ColoredVertex> ve = new List<ColoredVertex>();
+            float MaxUpdateTimes = 14 * Projectile.MaxUpdates;
+            float progress = (counter / MaxUpdateTimes);
+            float ofs = 0;
+            for (int i = 0; i < odr.Count; i++)
+            {
+                Color b = new Color(220, 200, 255);
+                ve.Add(new ColoredVertex(Projectile.Center - Main.screenPosition + (new Vector2(170 * Projectile.scale * odl[i], 0).RotatedBy(odr[i])),
+                      new Vector3(ofs, 1, 1),
+                      b));
+                ve.Add(new ColoredVertex(Projectile.Center - Main.screenPosition + (new Vector2(Projectile.scale * odl[i], 0).RotatedBy(odr[i])),
+                      new Vector3(ofs, 0, 1),
+                      b));
+                if (i < odr.Count - 1) {
+                    ofs += 1f / odr.Count;
+                }
+            }
+            if (ve.Count >= 3)
+            {
+                var gd = Main.graphics.GraphicsDevice;
+                SpriteBatch sb = Main.spriteBatch;
+                Effect shader = ModContent.Request<Effect>("CalamityEntropy/Assets/Effects/AntivoidTrail", AssetRequestMode.ImmediateLoad).Value;
+                sb.End();
+                sb.Begin(SpriteSortMode.Immediate, BlendState.NonPremultiplied, SamplerState.LinearWrap, DepthStencilState.None, RasterizerState.CullNone, null, Main.GameViewMatrix.TransformationMatrix);
+                shader.Parameters["alpha"].SetValue(1f - progress);
+                shader.Parameters["offset"].SetValue(rofs);
+                shader.CurrentTechnique.Passes["EffectPass"].Apply();
+                gd.Textures[1] = CEUtils.getExtraTex("MotionTrail3");
+                gd.Textures[0] = trail;
+                gd.Textures[2] = CEUtils.getExtraTex("Extra_202");
+                gd.DrawUserPrimitives(PrimitiveType.TriangleStrip, ve.ToArray(), 0, ve.Count - 2);
+                Main.spriteBatch.ExitShaderRegion();
+            }
+
+
+            int dir = (int)(Projectile.ai[0]);
+            Vector2 origin = dir > 0 ? new Vector2(0, tex.Height) : new Vector2(tex.Width, tex.Height);
+            SpriteEffects effect = dir > 0 ? SpriteEffects.None : SpriteEffects.FlipHorizontally;
+            float rot = dir > 0 ? Projectile.rotation + MathHelper.ToRadians(70 - 24) : Projectile.rotation + +MathHelper.ToRadians(110 + 24);
+
+
+            Main.EntitySpriteDraw(tex, Projectile.Center + Projectile.GetOwner().gfxOffY * Vector2.UnitY - Main.screenPosition, null, lightColor * alpha * (1 - progress), rot, origin, (Projectile.scale) * scale * lg, effect);
+
+            return false;
+        }
+        public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
+        {
+            return CEUtils.LineThroughRect(Projectile.Center, Projectile.Center + Projectile.rotation.ToRotationVector2() * (160 * lg) * Projectile.scale * scale, targetHitbox, 64);
+        }
+        public override void CutTiles()
+        {
+            Utils.PlotTileLine(Projectile.Center, Projectile.Center + Projectile.rotation.ToRotationVector2() * (160 * lg) * Projectile.scale * scale, 54, DelegateMethods.CutTiles);
         }
     }
 }
