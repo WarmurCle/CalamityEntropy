db47cb2f48c9a425683d5522dc18bd687d390585
Materialistic989286067 <3608728857@qq.com>
2025-07-11 23:48:40 +0800

    Update Light.cs

--- Content/ArmorPrefixes/Light.cs
+++ Content/ArmorPrefixes/Light.cs
@@ -6,8 +6,8 @@ namespace CalamityEntropy.Content.ArmorPrefixes
     {
         public override void UpdateEquip(Player player, Item item)
         {
-            player.Entropy().moveSpeed += 0.1f;
-            player.Entropy().WingSpeed += 0.1f;
+            player.Entropy().moveSpeed += 0.03f;
+            player.Entropy().WingSpeed += 0.05f;
         }
     }
 }
