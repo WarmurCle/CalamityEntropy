79ad8e8f3e31a76e9d74d4d6da0cc6ee79583d82
Polaris <2058770793@qq.com>
2025-07-12 18:32:51 +0800

    调整

--- Donators.cs
+++ Donators.cs
@@ -8,6 +8,6 @@ namespace CalamityEntropy
 {
     internal static class Donators
     {
-        public static List<string> Donors = new List<string>() {"冷级", "冻结",  "Polaris", "尘Dust", "烟玉", "心斩狂歌", "玲瓏", "ChaLost", "银九", "Kernschmelze", "Sora" };
+        public static List<string> Donors = new List<string>() {"冷级", "冻结",  "Polaris", "尘Dust", "烟玉", "心斩狂歌", "玲瓏", "ChaLost", "银九", "Kernschmelze", "Sora", "momodzmz", "锯角", "Away", "梦龍", "Badbf-HX", "Yharim", "Hocha", "Kikastorm", "Leon金蜂鸟", "Wuti-v-", "AtmospherePredator", "松饼" };
     }
 }
