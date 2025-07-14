0e8bfa312799dc38182ad8a1c2733799ad36898a
Materialistic989286067 <3608728857@qq.com>
2025-07-14 23:35:57 +0800

    Update Nyxolithraken.cs

--- Content/Items/Weapons/Nyxolithraken.cs
+++ Content/Items/Weapons/Nyxolithraken.cs
@@ -23,7 +23,7 @@ namespace CalamityEntropy.Content.Items.Weapons
 
         public override void SetDefaults()
         {
-            Item.damage = 2460;
+            Item.damage = 3000;
             Item.crit = 0;
             Item.DamageType = DamageClass.Summon;
             Item.width = 90;
