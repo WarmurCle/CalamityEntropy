f9c3efc3bc86593830304a58c792aac0e16a2bb2
Materialistic989286067 <3608728857@qq.com>
2025-07-21 22:15:29 +0800

    Update VoidFaquirCosmosHood.cs

--- Content/Items/Armor/VoidFaquir/VoidFaquirCosmosHood.cs
+++ Content/Items/Armor/VoidFaquir/VoidFaquirCosmosHood.cs
@@ -1,6 +1,7 @@
 ﻿using CalamityEntropy.Content.Rarities;
 using CalamityMod.Items;
 using CalamityMod.Items.Materials;
+using CalamityMod.Tiles.Furniture.CraftingStations;
 using System.Collections.Generic;
 using Terraria;
 using Terraria.ModLoader;
@@ -58,7 +59,9 @@ namespace CalamityEntropy.Content.Items.Armor.VoidFaquir
                 .AddIngredient(ModContent.ItemType<VoidBar>(), 14)
                 .AddIngredient(ModContent.ItemType<RuinousSoul>(), 6)
                 .AddIngredient(ModContent.ItemType<AscendantSpiritEssence>(), 6)
-                .AddIngredient(ModContent.ItemType<TwistingNether>(), 8).Register();
+                .AddIngredient(ModContent.ItemType<TwistingNether>(), 8)
+                .AddTile(ModContent.TileType<CosmicAnvil>())
+                .Register();
         }
     }
 }
