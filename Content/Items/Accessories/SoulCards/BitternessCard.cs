5759aebe5fa787989dede077e83196b6c36b86c8
Materialistic989286067 <3608728857@qq.com>
2025-07-29 16:39:30 +0800

    Update BitternessCard.cs

--- Content/Items/Accessories/SoulCards/BitternessCard.cs
+++ Content/Items/Accessories/SoulCards/BitternessCard.cs
@@ -9,7 +9,7 @@ namespace CalamityEntropy.Content.Items.Accessories.SoulCards
     public class BitternessCard : ModItem
     {
         public static float DmgMax = 0.12f;
-        public static float enduMax = 0.16f;
+        public static float enduMax = 0.10f;
 
         public override void SetDefaults()
         {
