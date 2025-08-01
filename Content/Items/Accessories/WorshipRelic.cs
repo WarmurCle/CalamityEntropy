34c56525cf181d34651e2d79a74afba194c76c6f
Materialistic989286067 <3608728857@qq.com>
2025-08-01 17:29:58 +0800

    Update WorshipRelic.cs

--- Content/Items/Accessories/WorshipRelic.cs
+++ Content/Items/Accessories/WorshipRelic.cs
@@ -18,7 +18,7 @@ namespace CalamityEntropy.Content.Items.Accessories
 {
     public class WorshipRelic : ModItem
     {
-        public static int ArrowDamage = 200;
+        public static int ArrowDamage = 160;
         public override void SetDefaults()
         {
             Item.width = 42;
