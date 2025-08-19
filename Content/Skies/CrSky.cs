764781f2bacf74cf6c2336098eec0ce09531e17a
HoCha113 <2188704314@qq.com>
2025-08-19 17:06:47 +0800

    修改渲染接口

--- Content/Skies/CrSky.cs
+++ Content/Skies/CrSky.cs
@@ -131,7 +131,7 @@ namespace CalamityEntropy.Content.Skies
             dp = new Vector2((Main.screenPosition.X * -0.5f * c + counter * -0.3f * c) % txd.Width, (Main.screenPosition.Y * -0.5f * c + counter * 0.1f * c) % txd.Height);
             spriteBatch.Draw(txd, new Vector2(-528, -528), new Rectangle((int)-dp.X, (int)-dp.Y, Main.screenWidth + 1024, Main.screenHeight + 1024), ocolor * opacity, 0, Vector2.Zero, 1, SpriteEffects.None, 0);
 
-            if (drawAWMask)
+            if (drawAWMask && EffectLoader.screen != null && EffectLoader.screen2 != null && !EffectLoader.screen.IsDisposed && !EffectLoader.screen2.IsDisposed)
             {
                 spriteBatch.End();
                 GraphicsDevice graphicsDevice = Main.graphics.GraphicsDevice;
