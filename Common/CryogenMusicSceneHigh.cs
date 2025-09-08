7f5e81ce99369ac4738912d7970e9606fe0f9728
Polaris <2058770793@qq.com>
2025-09-08 15:11:24 +0800

    1

--- Common/CryogenMusicSceneHigh.cs
+++ Common/CryogenMusicSceneHigh.cs
@@ -1,4 +1,5 @@
 ﻿using CalamityMod.NPCs.Cryogen;
+using CalamityMod.Systems;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -10,7 +11,7 @@ namespace CalamityEntropy.Common
         public override SceneEffectPriority Priority => (SceneEffectPriority)10;
 
         public override int NPCType => ModContent.NPCType<Cryogen>();
-        public override int? MusicModMusic => CalamityMod.Instance.GetMusicFromMusicMod("Cryogen");
+        public override int? MusicModMusic => ModContent.GetInstance<CalamityMod.CalamityMod>().GetMusicFromMusicMod("Cryogen");
         public override int VanillaMusic => MusicID.FrostMoon;
         public override int OtherworldMusic => MusicID.OtherworldlyIce;
     }
