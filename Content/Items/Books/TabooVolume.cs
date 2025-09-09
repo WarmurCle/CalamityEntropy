c93e37deba97b4f6c51f43236881ea3b0711d550
Materialistic989286067 <3608728857@qq.com>
2025-09-09 19:28:10 +0800

    Update TabooVolume.cs

--- Content/Items/Books/TabooVolume.cs
+++ Content/Items/Books/TabooVolume.cs
@@ -18,7 +18,7 @@ namespace CalamityEntropy.Content.Items.Books
         public override void SetDefaults()
         {
             base.SetDefaults();
-            Item.damage = 190;
+            Item.damage = 200;
             Item.useAnimation = Item.useTime = 100;
             Item.crit = 10;
             Item.mana = 8;
