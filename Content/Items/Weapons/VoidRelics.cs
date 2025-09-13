a832416820f00fb0f57ea43aaf0a666b7b395c0d
Materialistic989286067 <3608728857@qq.com>
2025-09-13 15:18:00 +0800

    Update VoidRelics.cs

--- Content/Items/Weapons/VoidRelics.cs
+++ Content/Items/Weapons/VoidRelics.cs
@@ -21,7 +21,7 @@ namespace CalamityEntropy.Content.Items.Weapons
 
         public override void SetDefaults()
         {
-            Item.damage = 86;
+            Item.damage = 75;
             Item.crit = 0;
             Item.DamageType = DamageClass.Summon;
             Item.width = 64;
