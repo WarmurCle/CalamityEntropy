3757f725925be3cb14a30a692904397ef144a81b
Polaris <2058770793@qq.com>
2025-09-28 14:38:55 +0800

    1

---
+++ Content/Items/SpiritDye.cs
@@ -0,0 +1,40 @@
+﻿using CalamityMod.Items.Dyes;
+using CalamityMod.Items.Placeables.Ores;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using Terraria;
+using Terraria.Graphics.Shaders;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityEntropy.Content.Items
+{
+    public class SpiritDye : BaseDye
+    {
+        public override ArmorShaderData ShaderDataToBind => new ArmorShaderData(Mod.Assets.Request<Effect>("Assets/Effects/SoulDiscorderDye"), "DyePass").
+            UseColor(new Color(200, 200, 255)).UseSecondaryColor(new Color(140, 140, 255)).UseImage(ModContent.Request<Texture2D>("CalamityEntropy/Assets/Extra/SoulDiscorderColorMap"));
+        public override void SafeSetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 3;
+        }
+
+        public override void SafeSetDefaults()
+        {
+            Item.rare = ItemRarityID.Orange;
+            Item.value = Item.sellPrice(0, 2, 50, 0);
+        }
+
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient<ProphetRelic>()
+                .AddTile(TileID.DyeVat)
+                .Register();
+            CreateRecipe(4)
+                .AddIngredient<ProphetTrophy>()
+                .AddTile(TileID.DyeVat)
+                .Register();
+        }
+    }
+}
