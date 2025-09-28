aa9c127cd1b13d46aaacac9fb947d16f21940aaf
Polaris <2058770793@qq.com>
2025-09-28 08:33:26 +0800

    1

---
+++ Content/Particles/HealingParticle.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria.ModLoader;
+
+namespace CalamityEntropy.Content.Particles
+{
+    public class HealingParticle : EParticle
+    {
+        public override Texture2D Texture => ModContent.Request<Texture2D>("CalamityEntropy/Content/Particles/HealingParticle").Value;
+        public override void OnSpawn()
+        {
+            this.Lifetime = 46;
+        }
+        public override void AI()
+        {
+            base.AI();
+            if(this.Lifetime < 26)
+                this.Opacity *= 0.9f;
+        }
+    }
+}
