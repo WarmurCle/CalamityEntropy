4145d6d4a0bd146106ce652f54b30170be124032
Materialistic989286067 <3608728857@qq.com>
2025-10-01 01:29:56 +0800

    Update Bramblecleave.cs

--- Content/Items/Weapons/GrassSword/Bramblecleave.cs
+++ Content/Items/Weapons/GrassSword/Bramblecleave.cs
@@ -156,14 +156,14 @@ namespace CalamityEntropy.Content.Items.Weapons.GrassSword
                 case 4: dmg = 55; break;
                 case 5: dmg = 80; break;
                 case 6: dmg = 90; break;
-                case 7: dmg = 110; break;
-                case 8: dmg = 130; break;
-                case 9: dmg = 180; break;
-                case 10: dmg = 350; break;
-                case 11: dmg = 500; break;
+                case 7: dmg = 120; break;
+                case 8: dmg = 150; break;
+                case 9: dmg = 200; break;
+                case 10: dmg = 400; break;
+                case 11: dmg = 600; break;
                 case 12: dmg = 1000; break;
-                case 13: dmg = 1250; break;
-                case 14: dmg = 1500; break;
+                case 13: dmg = 1450; break;
+                case 14: dmg = 1600; break;
                 case 15: dmg = 2400; break;
             }
             Item.damage = dmg;
