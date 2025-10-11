00d5b010c6d4d4a1ec1464c3d7a0edaf1fb3107d
Materialistic989286067 <3608728857@qq.com>
2025-10-11 23:26:10 +0800

    Update GrudgeCard.cs

--- Content/Items/Accessories/SoulCards/GrudgeCard.cs
+++ Content/Items/Accessories/SoulCards/GrudgeCard.cs
@@ -7,7 +7,7 @@ namespace CalamityEntropy.Content.Items.Accessories.SoulCards
 {
     public class GrudgeCard : ModItem
     {
-        public static float TempDefense = 2;
+        public static float TempDefense = 10;
         public override void SetDefaults()
         {
             Item.width = 22;
