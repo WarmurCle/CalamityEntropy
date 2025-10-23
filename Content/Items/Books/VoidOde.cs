f20191b467168d964383917d7ae72bf7222e45a0
Materialistic989286067 <3608728857@qq.com>
2025-10-23 20:39:38 +0800

    Update VoidOde.cs

--- Content/Items/Books/VoidOde.cs
+++ Content/Items/Books/VoidOde.cs
@@ -14,7 +14,7 @@ namespace CalamityEntropy.Content.Items.Books
         public override void SetDefaults()
         {
             base.SetDefaults();
-            Item.damage = 140;
+            Item.damage = 125;
             Item.useAnimation = Item.useTime = 7;
             Item.crit = 10;
             Item.mana = 6;
