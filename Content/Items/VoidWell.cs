a0d45a3d039905233ce0df07315fd64f723de716
Materialistic989286067 <3608728857@qq.com>
2025-10-30 22:30:46 +0800

    Update VoidWell.cs

--- Content/Items/VoidWell.cs
+++ Content/Items/VoidWell.cs
@@ -1,6 +1,7 @@
 ﻿using CalamityEntropy.Content.Rarities;
 using CalamityEntropy.Content.Tiles;
 using CalamityMod.Items.Materials;
+using CalamityMod.Tiles.Furniture.CraftingStations;
 using CalamityMod.Items.Placeables.Furniture.CraftingStations;
 using Terraria;
 using Terraria.ID;
