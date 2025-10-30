9a8223aa0091d071dede158cbe50b66df2baeb52
Materialistic989286067 <3608728857@qq.com>
2025-10-30 20:08:35 +0800

    Update VoidFaquirCosmosHood.cs

--- Content/Items/Armor/VoidFaquir/VoidFaquirCosmosHood.cs
+++ Content/Items/Armor/VoidFaquir/VoidFaquirCosmosHood.cs
@@ -1,6 +1,7 @@
 ﻿using CalamityEntropy.Content.Rarities;
 using CalamityMod.Items;
 using CalamityMod.Items.Materials;
+using CalamityMod.Tiles.Furniture.CraftingStations;
 using System.Collections.Generic;
 using Terraria;
 using Terraria.ID;
@@ -56,9 +57,8 @@ namespace CalamityEntropy.Content.Items.Armor.VoidFaquir
         {
             CreateRecipe()
                 .AddIngredient(ModContent.ItemType<VoidBar>(), 14)
-                .AddIngredient(ModContent.ItemType<RuinousSoul>(), 6)
                 .AddIngredient(ModContent.ItemType<TwistingNether>(), 8)
-                .AddTile(TileID.LunarCraftingStation)
+                .AddTile(ModContent.TileType<VoidWellTile>())
                 .Register();
         }
     }
