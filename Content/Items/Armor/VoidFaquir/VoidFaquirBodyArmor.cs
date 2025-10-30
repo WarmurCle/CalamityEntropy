86b93436bd34e49a5a519bbd71d43b79c0e43ae4
Materialistic989286067 <3608728857@qq.com>
2025-10-30 22:24:47 +0800

    Update VoidFaquirBodyArmor.cs

--- Content/Items/Armor/VoidFaquir/VoidFaquirBodyArmor.cs
+++ Content/Items/Armor/VoidFaquir/VoidFaquirBodyArmor.cs
@@ -1,4 +1,5 @@
 ﻿using CalamityEntropy.Content.Rarities;
+using CalamityEntropy.Content.Tiles;
 using CalamityMod.Items;
 using CalamityMod.Items.Materials;
 using CalamityMod.Tiles.Furniture.CraftingStations;
