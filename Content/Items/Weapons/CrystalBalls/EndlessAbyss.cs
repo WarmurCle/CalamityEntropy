9a72ecc1960942da0b5e58973f249365182147e7
Materialistic989286067 <3608728857@qq.com>
2025-10-30 20:38:18 +0800

    Update EndlessAbyss.cs

--- Content/Items/Weapons/CrystalBalls/EndlessAbyss.cs
+++ Content/Items/Weapons/CrystalBalls/EndlessAbyss.cs
@@ -1,6 +1,7 @@
 ﻿using CalamityEntropy.Content.Projectiles;
 using CalamityEntropy.Content.Rarities;
 using CalamityMod.Items;
+using CalamityMod.Tiles.Furniture.CraftingStations;
 using Terraria;
 using Terraria.DataStructures;
 using Terraria.ID;
@@ -40,9 +41,9 @@ namespace CalamityEntropy.Content.Items.Weapons.CrystalBalls
         public override void AddRecipes()
         {
             CreateRecipe()
-                .AddIngredient(ModContent.ItemType<VoidBar>(), 10)
                 .AddIngredient(ItemID.CrystalBall, 1)
-                .AddTile(TileID.LunarCraftingStation)
+                .AddIngredient(ModContent.ItemType<VoidBar>(), 5)
+                .AddTile(ModContent.TileType<VoidWellTile>())
                 .Register();
         }
         public override bool MagicPrefix()
