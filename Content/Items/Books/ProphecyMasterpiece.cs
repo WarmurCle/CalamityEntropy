4027685081505b30eef42e5e257833f5221b6e32
Materialistic989286067 <3608728857@qq.com>
2025-11-01 11:57:41 +0800

    Update ProphecyMasterpiece.cs

--- Content/Items/Books/ProphecyMasterpiece.cs
+++ Content/Items/Books/ProphecyMasterpiece.cs
@@ -19,9 +19,9 @@ namespace CalamityEntropy.Content.Items.Books
         {
             base.SetDefaults();
             Item.damage = 70;
-            Item.useAnimation = Item.useTime = 16;
+            Item.useAnimation = Item.useTime = 11;
             Item.crit = 15;
-            Item.mana = 8;
+            Item.mana = 5;
             Item.rare = ItemRarityID.Yellow;
             Item.expert = true;
             Item.value = CalamityGlobalItem.RarityYellowBuyPrice;
