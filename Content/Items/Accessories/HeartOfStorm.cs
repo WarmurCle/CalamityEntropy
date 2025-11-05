47288e88dcd59af3895fed4f0544153b74836ff1
HoCha113 <127948319+hocha113@users.noreply.github.com>
2025-11-05 22:45:24 +0800

    Add localization and Calamity Overhaul synergy
    Added a localized tooltip for the Heart of Storm accessory with dynamic color effects when Calamity Overhaul is detected. Updated localization files with new translations for "CWRGanged" and additional ammo types. Removed unused recipe method.

--- Content/Items/Accessories/HeartOfStorm.cs
+++ Content/Items/Accessories/HeartOfStorm.cs
@@ -1,12 +1,21 @@
 ﻿using CalamityEntropy.Content.Rarities;
 using CalamityMod.Items;
+using InnoVault;
+using System.Collections.Generic;
 using Terraria;
+using Terraria.Localization;
 using Terraria.ModLoader;
 
 namespace CalamityEntropy.Content.Items.Accessories
 {
     public class HeartOfStorm : ModItem
     {
+        public static LocalizedText CWRGanged;
+        public override void SetStaticDefaults()
+        {
+            CWRGanged = this.GetLocalization("CWRGanged", () => "灾厄大修联动效果:召唤风暴女神残魂配合你作战");
+        }
+
         public override void SetDefaults()
         {
             Item.width = 52;
@@ -14,7 +23,6 @@ namespace CalamityEntropy.Content.Items.Accessories
             Item.value = CalamityGlobalItem.RarityRedBuyPrice;
             Item.rare = ModContent.RarityType<GlowPurple>();
             Item.accessory = true;
-
         }
 
         public override void UpdateAccessory(Player player, bool hideVisual)
@@ -22,8 +30,15 @@ namespace CalamityEntropy.Content.Items.Accessories
             player.Entropy().heartOfStorm = true;
         }
 
-        public override void AddRecipes()
+        public override void ModifyTooltips(List<TooltipLine> tooltips)
         {
+            if (ModLoader.HasMod("CalamityOverhaul")) 
+            {
+                var line = new TooltipLine(Mod, "CWRGanged", CWRGanged.Value);
+                line.OverrideColor = VaultUtils.MultiStepColorLerp(Main.LocalPlayer.miscCounter % 300 / 300f
+                    , Color.AliceBlue, Color.BlueViolet, Color.White, Color.BlueViolet, Color.AliceBlue);
+                tooltips.Add(line);
+            }
         }
     }
 }
