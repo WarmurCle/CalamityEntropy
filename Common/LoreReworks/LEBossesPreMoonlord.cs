a6712223f3f072f39aee2c83c80dd59e7b8a6f39
Polaris <2058770793@qq.com>
2025-11-06 20:30:01 +0800

    LE

--- Common/LoreReworks/LEBossesPreMoonlord.cs
+++ Common/LoreReworks/LEBossesPreMoonlord.cs
@@ -1,4 +1,6 @@
-﻿using CalamityMod.Items.LoreItems;
+﻿using CalamityMod;
+using CalamityMod.Buffs.StatDebuffs;
+using CalamityMod.Items.LoreItems;
 using Terraria;
 using Terraria.ID;
 using Terraria.ModLoader;
@@ -16,6 +18,36 @@ namespace CalamityEntropy.Common.LoreReworks
         {
             tooltip.Text = tooltip.Text.Replace("{1}", DmgReduce.ToPercent().ToString());
             tooltip.Text = tooltip.Text.Replace("{2}", Cooldown.ToString());
+        }
+    }
+    public class LEQueenSlime : LoreEffect
+    {
+        public override int ItemType => ModContent.ItemType<LoreQueenSlime>();
+        public static float FallingSpeedAdd = 0.05f;
+        public override void ModifyTooltip(TooltipLine tooltip)
+        {
+            tooltip.Text = tooltip.Text.Replace("{1}", FallingSpeedAdd.ToPercent().ToString());
+        }
+        public override void UpdateEffects(Player player)
+        {
+            player.Entropy().FallSpeed += FallingSpeedAdd;
+        }
+    }
+    public class LECryo : LoreEffect
+    {
+        public override int ItemType => ModContent.ItemType<LoreArchmage>();
+        public override void UpdateEffects(Player player)
+        {
+            for (int b = 0; b < Player.MaxBuffs; b++)
+            {
+                if (player.buffType[b] == BuffID.Chilled || player.buffType[b] == BuffID.Frozen || player.buffType[b] == ModContent.BuffType<GlacialState>())
+                {
+                    if (player.buffTime[b] > 2 && Main.GameUpdateCount % 2 == 0)
+                    {
+                        player.buffTime[b] -= 1;
+                    }
+                }
+            }
         }
     }
 }
