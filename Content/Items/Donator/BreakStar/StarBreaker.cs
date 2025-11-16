3daca0ddd299a18b7d2ca139fc95178b8469dc25
Polaris <2058770793@qq.com>
2025-11-16 23:13:21 +0800

    捐赠者物品

---
+++ Content/Items/Donator/BreakStar/StarBreaker.cs
@@ -0,0 +1,281 @@
+using CalamityEntropy.Content.Items.Weapons.Fractal;
+using CalamityEntropy.Content.Particles;
+using CalamityEntropy.Content.Tiles;
+using CalamityEntropy.Utilities;
+using CalamityMod;
+using CalamityMod.Buffs.StatDebuffs;
+using CalamityMod.Items;
+using CalamityMod.Items.Materials;
+using CalamityMod.Items.Weapons.Melee;
+using CalamityMod.Particles;
+using CalamityMod.Projectiles.Rogue;
+using CalamityMod.Tiles.Furniture.CraftingStations;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using System;
+using System.Collections.Generic;
+using System.Runtime.Intrinsics.Arm;
+using Terraria;
+using Terraria.Audio;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityEntropy.Content.Items.Donator.BreakStar
+{
+    public class StarBreaker : ModItem, IDevItem
+    {
+        public string DevName => "锯角";
+
+        public override bool PreDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI)
+        {
+            Texture2D tex = TextureAssets.Item[Type].Value;
+            Vector2 position = Item.position - Main.screenPosition + tex.Size() / 2;
+            Rectangle iFrame = tex.Frame();
+            for (int i = 0; i < 16; i++)
+                spriteBatch.Draw(tex, position + MathHelper.ToRadians(i * 60f).ToRotationVector2() * 2f, null, Color.Blue with { A = 0 }, rotation, tex.Size() / 2, scale, 0, 0f);
+
+            spriteBatch.Draw(tex, position, iFrame, Color.White, rotation, tex.Size() / 2, scale, 0f, 0f);
+            Lighting.AddLight(position, TorchID.Blue);
+            return false;
+        }
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient<Nadir>()
+                .AddIngredient<FadingRunestone>()
+                .AddIngredient<GalacticaSingularity>(4)
+                .AddTile<VoidWellTile>()
+                .Register();
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 256;
+            Item.height = 256;
+            Item.damage = 3600;
+            Item.noMelee = true;
+            Item.noUseGraphic = true;
+            Item.useAnimation = Item.useTime = 20;
+            Item.useStyle = ItemUseStyleID.Shoot;
+            Item.knockBack = 1f;
+            Item.UseSound = SoundID.Item1;
+            Item.maxStack = 1;
+            Item.value = CalamityGlobalItem.RarityCalamityRedBuyPrice;
+            Item.rare = ItemRarityID.Blue;
+            Item.shoot = ModContent.ProjectileType<StarBreakerHeld>();
+            Item.shootSpeed = 42;
+            Item.DamageType = DamageClass.Melee;
+            Item.channel = true;
+            Item.crit = 12;
+        }
+    }
+    public class StarBreakerHeld : ModProjectile
+    {
+        public override void SetDefaults()
+        {
+            Projectile.FriendlySetDefaults(DamageClass.Melee, false, -1);
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = -1;
+            Projectile.timeLeft = 60;
+        }
+        public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
+        {
+            return CEUtils.LineThroughRect(Projectile.Center + Projectile.rotation.ToRotationVector2() * 480 * Projectile.scale, Projectile.Center, targetHitbox, 140);
+        }
+        public Rope rope = null;
+        public int AttackCount = 0;
+        public float AttackTime = 0;
+        public int AttackDelay = 0;
+        public float RotP = 0;
+        public float BaseScale = 0;
+        public float num = 0;
+        public override void AI()
+        {
+            Player player = Projectile.GetOwner();
+            Projectile.StickToPlayer();
+
+            Projectile.rotation += RotP;
+            Projectile.Center = player.HandPosition.Value;
+            Projectile.Center += Projectile.rotation.ToRotationVector2() * -160 * Projectile.scale;
+            player.direction = (Projectile.rotation.ToRotationVector2().X > 0 ? 1 : -1);
+            player.itemRotation = (Projectile.velocity * player.direction).ToRotation();
+            if (BaseScale == 0)
+                BaseScale = Projectile.scale;
+            
+            if (player.channel)
+            {
+                player.itemTime = player.itemAnimation = 3;
+                Projectile.timeLeft = 3;
+                if (AttackCount % 4 == 3 && AttackDelay <= 0)
+                {
+                    Projectile.scale = BaseScale * 1.5f;
+                }
+                else
+                {
+                    Projectile.scale = BaseScale;
+                }
+                AttackDelay--;
+                if(AttackDelay < 0 || AttackTime > 0)
+                {
+                    int ItemTime = player.HeldItem.useTime;
+                    float add = player.GetTotalAttackSpeed(Projectile.DamageType) * (1f / ItemTime) * (AttackCount % 4 == 3 ? 2 : 3f);
+                    if (AttackTime == 0)
+                    {
+                        for (int i = 0; i < 4; i++)
+                            SoundEngine.PlaySound(SoundID.Item1 with { MaxInstances = 10}, Projectile.Center);
+                        if (!(AttackCount % 4 == 3))
+                            RotP = Main.rand.NextFloat(-0.3f, 0.3f);
+                        num = (RotP * -2f) / (1 / add);
+                        Projectile.ResetLocalNPCHitImmunity();
+                        PlaySound = false;
+                        GeneralParticleHandler.SpawnParticle(new DirectionalPulseRing(Projectile.Center + Projectile.velocity.normalize() * 500 * Projectile.scale, player.velocity + Projectile.velocity.normalize() * -32 * Projectile.scale, new Color(255, 80, 80), new Vector2(0.3f, 1), Projectile.velocity.ToRotation(), 0.1f, 1f * Projectile.scale, 16));
+                    }
+                    RotP += num;
+                    AttackTime += add;
+                    
+                    if (AttackTime > 1)
+                        AttackTime = 1; 
+                    Projectile.Center += Projectile.rotation.ToRotationVector2() * 150 * Projectile.scale * CEUtils.Parabola(AttackTime, 1);//(AttackTime > 0.5f ? (1 - (AttackTime - 0.5f) * 2) : AttackTime * 2);
+                    
+                    if (AttackTime >= 1)
+                    {
+                        AttackTime = 0;
+                        AttackCount++;
+                        if (AttackCount % 4 == 3 || AttackCount % 4 == 0)
+                            AttackDelay = 5;
+                    }
+                }
+                else { RotP = 0; }
+            }
+            else
+            {
+                Projectile.Kill();
+                return;
+            }
+
+
+            Vector2 ropePoint = Projectile.Center + Projectile.rotation.ToRotationVector2() * 234 * Projectile.scale;
+            if (rope == null)
+            {
+                rope = new Rope(ropePoint, 20, 5.4f * Projectile.scale, new Vector2(0, 0.5f), 0.35f, 64);
+            }
+            Vector2 lst = rope.Start;
+            rope.gravity = Projectile.rotation.ToRotationVector2().RotatedBy((float)Math.Sin(Main.GameUpdateCount * 0.025f) * 0.5f) * -0.4f;
+            for (float r = 0.5f; r <= 1; r += 0.5f)
+            {
+                rope.Start = Vector2.Lerp(lst, ropePoint, r);
+                rope.Update();
+            }
+            var points = rope.GetPoints();
+            odp.Clear();
+            odp.Add(points[0]);
+            for (int i = 1; i < points.Count; i++)
+            {
+                for (float j = 0.25f; j <= 1f; j += 0.25f)
+                {
+                    odp.Add(Vector2.Lerp(points[i - 1], points[i], j));
+                }
+
+            }
+        }
+        public List<Vector2> odp = new();
+        public bool PlaySound = false;
+        public override void ModifyHitNPC(NPC target, ref NPC.HitModifiers modifiers)
+        {
+            modifiers.ArmorPenetration += 80 + target.defense / 2;
+            if (AttackCount % 4 == 3)
+            {
+                modifiers.SetCrit();
+                modifiers.SourceDamage *= 3;
+                modifiers.ArmorPenetration += 200;
+            }
+        }
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            target.AddBuff<MarkedforDeath>(180);
+
+            if (!PlaySound)
+            {
+                CEUtils.SetShake(target.Center, 4 * Projectile.scale);
+                PlaySound = true;
+                CEUtils.PlaySound("spearImpact", Main.rand.NextFloat(0.8f, 1.4f), target.Center);
+            }
+            for(int i = 0; i < 32; i++)
+            {
+                float sparkScale2 = Main.rand.NextFloat(1.4f, 2.4f);
+                Vector2 sparkVelocity2 = Projectile.rotation.ToRotationVector2().RotatedByRandom(0.2f) * 64 * Main.rand.NextFloat(0.2f, 1);
+                if (Main.rand.NextBool(5))
+                {
+                    AltSparkParticle spark = new AltSparkParticle(target.Center + Main.rand.NextVector2Circular(target.width * 0.5f, target.height * 0.5f), sparkVelocity2 * (1f), false, 12, sparkScale2 * (1.4f), Color.Crimson);
+                    GeneralParticleHandler.SpawnParticle(spark);
+                }
+                else
+                {
+                    LineParticle spark = new LineParticle(target.Center + Main.rand.NextVector2Circular(target.width * 0.5f, target.height * 0.5f), sparkVelocity2, false, 8, sparkScale2 * (Projectile.frame == 7 ? 1.4f : 1f), Main.rand.NextBool() ? Color.Red : new Color(255, 190, 190));
+                    GeneralParticleHandler.SpawnParticle(spark);
+                }
+            }
+        }
+        public override bool PreDraw(ref Color lightColor)
+        {
+            if(odp != null)
+            {
+                float w = 7 * Projectile.scale;
+                int xp = 0;
+                List<ColoredVertex> ve = new List<ColoredVertex>();
+                Color b = Lighting.GetColor((int)(Projectile.Center.X / 16f), (int)(Projectile.Center.Y / 16f));
+                b = b * Projectile.GetOwner().Entropy().alpha;
+                ve.Add(new ColoredVertex(new Vector2(xp, 0) + odp[0] - Main.screenPosition + (odp[1] - odp[0]).ToRotation().ToRotationVector2().RotatedBy(MathHelper.ToRadians(90)) * w,
+                          new Vector3((float)0, 1, 1),
+                          b));
+                ve.Add(new ColoredVertex(new Vector2(xp, 0) + odp[0] - Main.screenPosition + (odp[1] - odp[0]).ToRotation().ToRotationVector2().RotatedBy(MathHelper.ToRadians(-90)) * w,
+                      new Vector3((float)0, 0, 1),
+                      b));
+                for (int i = 1; i < odp.Count; i++)
+                {
+                    ve.Add(new ColoredVertex(new Vector2(xp, 0) + odp[i] - Main.screenPosition + (odp[i] - odp[i - 1]).ToRotation().ToRotationVector2().RotatedBy(MathHelper.ToRadians(90)) * w,
+                          new Vector3((float)(i + 1) / odp.Count, 1, 1),
+                          b));
+                    ve.Add(new ColoredVertex(new Vector2(xp, 0) + odp[i] - Main.screenPosition + (odp[i] - odp[i - 1]).ToRotation().ToRotationVector2().RotatedBy(MathHelper.ToRadians(-90)) * w,
+                          new Vector3((float)(i + 1) / odp.Count, 0, 1),
+                          b));
+
+                }
+                Texture2D texst = this.getTextureAlt("String");
+                var gd = Main.graphics.GraphicsDevice;
+                gd.Textures[0] = texst;
+                gd.DrawUserPrimitives(PrimitiveType.TriangleStrip, ve.ToArray(), 0, ve.Count - 2);
+            }
+            Main.spriteBatch.UseBlendState(BlendState.NonPremultiplied);
+            var tex = Projectile.GetTexture();
+            Main.spriteBatch.Draw(tex, Projectile.Center - Main.screenPosition, null, Color.White, Projectile.rotation + MathHelper.PiOver4, new Vector2(32, tex.Height - 32), Projectile.scale, 0f, 0f);
+            Texture2D arrow = CEUtils.getExtraTex("SpearArrow");
+            float arrowAlpha = CEUtils.Parabola(AttackTime, 1) - 0.6f;
+            if (arrowAlpha < 0)
+                arrowAlpha = 0;
+            arrowAlpha /= 0.4f;
+            arrowAlpha *= 0.7f;
+            Color applyAlpha(Color bc)
+            {
+                return new Color(bc.R, bc.G, bc.B, (int)(255 * arrowAlpha));
+            }
+            for(int i = 0; i < 2; i++)
+            {
+                Main.spriteBatch.Draw(arrow, Projectile.Center - Main.screenPosition + Projectile.rotation.ToRotationVector2() * 330 + CEUtils.randomPointInCircle(24), null, applyAlpha(Color.Red), Projectile.rotation + Main.rand.NextFloat(-0.22f, 0.22f), arrow.Size() / 2f, new Vector2(2f, 1) * Projectile.scale * 0.4f, SpriteEffects.None, 0);
+            }
+            Main.spriteBatch.Draw(arrow, Projectile.Center - Main.screenPosition + Projectile.rotation.ToRotationVector2() * 360, null, applyAlpha(Color.Red), Projectile.rotation, arrow.Size() / 2f, new Vector2(1.8f, 1) * Projectile.scale * 0.6f * Projectile.scale, SpriteEffects.None, 0);
+            Main.spriteBatch.Draw(arrow, Projectile.Center - Main.screenPosition + Projectile.rotation.ToRotationVector2() * 360, null, applyAlpha(Color.White) * 2, Projectile.rotation, arrow.Size() / 2f, new Vector2(1.7f, 0.4f) * Projectile.scale * 0.6f * Projectile.scale, SpriteEffects.None, 0);
+            Main.spriteBatch.Draw(arrow, Projectile.Center - Main.screenPosition + Projectile.rotation.ToRotationVector2() * 440, null, applyAlpha(Color.White), Projectile.rotation, arrow.Size() / 2f, new Vector2(1.8f, 1f) * Projectile.scale * 0.24f * Projectile.scale, SpriteEffects.None, 0);
+
+            Main.spriteBatch.ExitShaderRegion();
+            List<Vector2> points = new();
+            for(int i = AttackCount % 4 == 3 ? 100 : 250; i <= 450; i += AttackCount % 4 == 3 ? 50 : 25)
+            {
+                points.Add(Projectile.Center + Projectile.rotation.ToRotationVector2() * i * Projectile.scale + Projectile.rotation.ToRotationVector2().RotatedBy(MathHelper.PiOver2) * Main.rand.Next(-60, 61) * ((450 - i) / (AttackCount % 4 != 3 ? 200f : 350f)));
+            }
+            CEUtils.DrawLines(points, Color.White * (arrowAlpha / 0.5f), 2 * Projectile.scale);
+            return false;
+        }
+    }
+}
---
+++ Content/Items/Donator/Gungnir.cs
@@ -0,0 +1,207 @@
+using CalamityEntropy.Content.Items.Weapons.Fractal;
+using CalamityEntropy.Content.Particles;
+using CalamityMod;
+using CalamityMod.Items;
+using CalamityMod.Items.Materials;
+using CalamityMod.Projectiles.Rogue;
+using CalamityMod.Tiles.Furniture.CraftingStations;
+using Microsoft.Xna.Framework.Graphics;
+using ReLogic.Content;
+using System.Collections.Generic;
+using Terraria;
+using Terraria.GameContent;
+using Terraria.ID;
+using Terraria.ModLoader;
+using static Terraria.GameContent.Bestiary.IL_BestiaryDatabaseNPCsPopulator.CommonTags.SpawnConditions;
+
+namespace CalamityEntropy.Content.Items.Donator
+{
+    public class Gungnir : ModItem, IDonatorItem
+    {
+        public string DonatorName => "散尘化天心";
+
+        public override bool PreDrawInWorld(SpriteBatch spriteBatch, Color lightColor, Color alphaColor, ref float rotation, ref float scale, int whoAmI)
+        {
+            Texture2D tex = TextureAssets.Item[Type].Value;
+            Vector2 position = Item.position - Main.screenPosition + tex.Size() / 2;
+            Rectangle iFrame = tex.Frame();
+            for (int i = 0; i < 16; i++)
+                spriteBatch.Draw(tex, position + MathHelper.ToRadians(i * 60f).ToRotationVector2() * 2f, null, Color.DarkBlue with { A = 0 }, rotation, tex.Size() / 2, scale, 0, 0f);
+
+            spriteBatch.Draw(tex, position, iFrame, Color.White, rotation, tex.Size() / 2, scale, 0f, 0f);
+            Lighting.AddLight(position, TorchID.Blue);
+            return false;
+        }
+        public override void AddRecipes()
+        {
+            CreateRecipe()
+                .AddIngredient(ItemID.Gungnir)
+                .AddIngredient<DivineGeode>(12)
+                .AddIngredient<GalacticaSingularity>(6)
+                .AddIngredient<AscendantSpiritEssence>(2)
+                .AddIngredient<CosmiliteBar>(12)
+                .AddTile<CosmicAnvil>()
+                .Register();
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 120;
+            Item.height = 120;
+            Item.damage = 1300;
+            Item.noMelee = true;
+            Item.noUseGraphic = true;
+            Item.useAnimation = Item.useTime = 16;
+            Item.useStyle = ItemUseStyleID.Swing;
+            Item.knockBack = 1f;
+            Item.UseSound = SoundID.Item1;
+            Item.autoReuse = true;
+            Item.maxStack = 1;
+            Item.value = CalamityGlobalItem.RarityCalamityRedBuyPrice;
+            Item.rare = ItemRarityID.Blue;
+            Item.shoot = ModContent.ProjectileType<GungnirThrow>();
+            Item.shootSpeed = 42;
+            Item.DamageType = DamageClass.Melee;
+        }
+    }
+    public class GungnirThrow : ModProjectile
+    {
+        public override string Texture => "CalamityEntropy/Content/Items/Donator/Gungnir";
+        public override void SetDefaults()
+        {
+            Projectile.FriendlySetDefaults(DamageClass.Melee, false, -1);
+            Projectile.usesLocalNPCImmunity = true;
+            Projectile.localNPCHitCooldown = -1;
+            Projectile.MaxUpdates = 4;
+            Projectile.timeLeft = 60;
+        }
+        public override bool? Colliding(Rectangle projHitbox, Rectangle targetHitbox)
+        {
+            return CEUtils.LineThroughRect(Projectile.Center - Projectile.rotation.ToRotationVector2() * 170 * Projectile.scale, Projectile.Center, targetHitbox, 32);
+        }
+        public List<Vector2> oldPos = new List<Vector2>();
+        public List<float> oldRot = new List<float>();
+        public override void AI()
+        {
+            if (Projectile.localAI[0]++ == 0)
+                CEUtils.PlaySound("xswing", Main.rand.NextFloat(3, 3.6f), Projectile.Center);
+            Projectile.rotation = Projectile.velocity.ToRotation();
+            oldPos.Add(Projectile.Center);
+            oldRot.Add(Projectile.rotation);
+            if (oldPos.Count > 36)
+            {
+                oldPos.RemoveAt(0);
+                oldRot.RemoveAt(0);
+            }
+            if (Projectile.localAI[0] > 1)
+            {
+                for (float i = 0; i < 1; i += 1f)
+                {
+                    EParticle.spawnNew(new HeavenfallStar2() { drawScale = new Vector2(0.4f, 3) }, Projectile.Center - Projectile.velocity * i, Projectile.velocity * 0.6f, new Color(60, 60, 255), 0.4f, 1, true, BlendState.Additive, Projectile.rotation, 5);
+                }
+            }
+        }
+
+        public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
+        {
+            EParticle.spawnNew(new DOracleSlash() { centerColor = Color.White }, Projectile.Center - Projectile.velocity * 0.8f, Vector2.Zero, new Color(122, 122, 255), Main.rand.NextFloat(380, 420), 1f, true, BlendState.Additive, Projectile.rotation + MathHelper.Pi, 8);
+            if (Projectile.ai[1]-- > -3)
+            {
+                CEUtils.PlaySound("ThunderStrike", Main.rand.NextFloat(0.8f, 1.2f), target.Center, 6, 0.4f);
+                CEUtils.PlaySound("ystn_hit", 2.7f, target.Center);
+                for (int i = 0; i < 8; i++)
+                {
+                    Vector2 pos = target.Center + new Vector2(0, -900) + CEUtils.randomPointInCircle(600);
+                    Projectile.NewProjectile(Projectile.GetSource_FromThis(), pos, (target.Center - pos).normalize() * 42, ModContent.ProjectileType<AstralStarMelee>(), Projectile.damage / 4, Projectile.owner);
+                }
+                for (int i = 0; i < 1; i++)
+                {
+                    int lightningDamage = (int)(Projectile.damage * 0.6f);
+                    Vector2 lightningSpawnPosition = Projectile.Center - Vector2.UnitY.RotatedByRandom(0.2f) * 1000f;
+                    Vector2 lightningShootVelocity = (target.Center - lightningSpawnPosition + target.velocity * 7.5f).SafeNormalize(Vector2.UnitY) * 15f;
+                    int lightning = Projectile.NewProjectile(Projectile.GetSource_FromThis(), lightningSpawnPosition, lightningShootVelocity, ModContent.ProjectileType<StormfrontLightning>(), lightningDamage, 0f, Projectile.owner);
+                    if (Main.projectile.IndexInRange(lightning))
+                    {
+                        Main.projectile[lightning].CritChance = Projectile.CritChance;
+                        Main.projectile[lightning].ai[0] = lightningShootVelocity.ToRotation();
+                        Main.projectile[lightning].ai[1] = Main.rand.Next(100);
+                        Main.projectile[lightning].DamageType = Projectile.DamageType;
+                    }
+                }
+            }
+        }
+        public override bool PreDraw(ref Color lightColor)
+        {
+            Effect shader = ModContent.Request<Effect>("CalamityEntropy/Assets/Effects/SwordTrail3", AssetRequestMode.ImmediateLoad).Value;
+            List<ColoredVertex> ve = new();
+            {
+                for (int i = 0; i < oldPos.Count; i++)
+                {
+                    Color b = new Color(255, 255, 255);
+                    ve.Add(new ColoredVertex(oldPos[i] - Main.screenPosition + (new Vector2(12 * Projectile.scale * 1, 0).RotatedBy(oldRot[i] + MathHelper.PiOver2)),
+                          new Vector3((i) / ((float)oldPos.Count - 1), 1, 1),
+                          b));
+                    ve.Add(new ColoredVertex(oldPos[i] - Main.screenPosition + (new Vector2(12 * Projectile.scale * 1, 0).RotatedBy(oldRot[i] - MathHelper.PiOver2)),
+                          new Vector3((i) / ((float)oldPos.Count - 1), 0, 1),
+                          b));
+                }
+                if (ve.Count >= 3)
+                {
+                    var gd = Main.graphics.GraphicsDevice;
+                    SpriteBatch sb = Main.spriteBatch;
+
+                    sb.End();
+                    sb.Begin(SpriteSortMode.Immediate, BlendState.NonPremultiplied, SamplerState.LinearWrap, DepthStencilState.None, RasterizerState.CullNone, shader, Main.GameViewMatrix.TransformationMatrix);
+                    shader.Parameters["color1"].SetValue(Color.DarkBlue.ToVector4());
+                    shader.Parameters["color2"].SetValue(new Color(220, 220, 255).ToVector4());
+                    shader.Parameters["uTime"].SetValue(Main.GlobalTimeWrappedHourly * 2.4f);
+                    shader.Parameters["alpha"].SetValue(1);
+                    shader.CurrentTechnique.Passes["EffectPass"].Apply();
+                    gd.Textures[0] = CEUtils.getExtraTex("Streak2");
+                    gd.DrawUserPrimitives(PrimitiveType.TriangleStrip, ve.ToArray(), 0, ve.Count - 2);
+                    Main.spriteBatch.ExitShaderRegion();
+                }
+            }
+
+            ve.Clear();
+            {
+                for (int i = 0; i < oldPos.Count; i++)
+                {
+                    Color b = new Color(255, 255, 255);
+                    ve.Add(new ColoredVertex(oldPos[i] - Main.screenPosition + (new Vector2(9 * Projectile.scale * 1, 0).RotatedBy(oldRot[i] + MathHelper.PiOver2)),
+                          new Vector3((i) / ((float)oldPos.Count - 1), 1, 1),
+                          b));
+                    ve.Add(new ColoredVertex(oldPos[i] - Main.screenPosition + (new Vector2(9 * Projectile.scale * 1, 0).RotatedBy(oldRot[i] - MathHelper.PiOver2)),
+                          new Vector3((i) / ((float)oldPos.Count - 1), 0, 1),
+                          b));
+                }
+                if (ve.Count >= 3)
+                {
+                    var gd = Main.graphics.GraphicsDevice;
+                    SpriteBatch sb = Main.spriteBatch;
+
+                    sb.End();
+                    sb.Begin(SpriteSortMode.Immediate, BlendState.Additive, SamplerState.LinearWrap, DepthStencilState.None, RasterizerState.CullNone, shader, Main.GameViewMatrix.TransformationMatrix);
+                    shader.Parameters["color1"].SetValue(new Vector4(1, 1, 1, 0));
+                    shader.Parameters["color2"].SetValue(Color.White.ToVector4() * 0.82f);
+                    shader.Parameters["uTime"].SetValue(Main.GlobalTimeWrappedHourly * 3f);
+                    shader.Parameters["alpha"].SetValue(1);
+                    shader.CurrentTechnique.Passes["EffectPass"].Apply();
+                    gd.Textures[0] = CEUtils.getExtraTex("Streak1");
+                    gd.DrawUserPrimitives(PrimitiveType.TriangleStrip, ve.ToArray(), 0, ve.Count - 2);
+                    Main.spriteBatch.ExitShaderRegion();
+                }
+            }
+
+            Main.spriteBatch.UseBlendState(BlendState.AlphaBlend);
+            Texture2D tex = Projectile.GetTexture();
+            Vector2 position = Projectile.Center - Main.screenPosition;
+            for (int i = 0; i < 16; i++)
+                Main.spriteBatch.Draw(tex, position + MathHelper.ToRadians(i * 60f).ToRotationVector2() * 2f, null, Color.Blue with { A = 0 }, Projectile.rotation + MathHelper.PiOver4, tex.Size() / 2, Projectile.scale, 0, 0f);
+
+            Main.spriteBatch.Draw(tex, position, null, Color.White, Projectile.rotation + MathHelper.PiOver4, tex.Size() / 2, Projectile.scale, 0f, 0f);
+            Lighting.AddLight(position, TorchID.Blue);
+            return false;
+        }
+    }
+}
