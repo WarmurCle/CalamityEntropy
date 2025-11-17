fff03c888de237aabee56d6a388e6ffd5c767c03
Polaris <2058770793@qq.com>
2025-11-17 22:24:09 +0800

    -

---
+++ Content/Buffs/Pets/ChaosTyrant.cs
@@ -0,0 +1,20 @@
+﻿using CalamityEntropy.Content.Projectiles.Pets;
+using Terraria;
+using Terraria.ModLoader;
+
+namespace CalamityEntropy.Content.Buffs.Pets
+{
+    public class ChaosTyrant : ModBuff
+    {
+        public override void SetStaticDefaults()
+        {
+            Main.buffNoTimeDisplay[Type] = true;
+            Main.vanityPet[Type] = true;
+        }
+        public override void Update(Player player, ref int buffIndex)
+        {
+            bool unused = false;
+            player.BuffHandle_SpawnPetIfNeededAndSetTime(buffIndex, ref unused, ModContent.ProjectileType<Sorensen>());
+        }
+    }
+}
