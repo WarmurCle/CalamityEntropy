0ac074c0626890de3c4b943c760881cc8f285daf
HoCha113 <127948319+hocha113@users.noreply.github.com>
2025-11-19 23:22:24 +0800

    Create TheIntimateLifeofTheHalibutAndTheNetherdrake.cs

---
+++ Content/OtherMods/CalamityOverhaul/TheIntimateLifeofTheHalibutAndTheNetherdrake.cs
@@ -0,0 +1,31 @@
+﻿using CalamityOverhaul.Content.ADV;
+using Microsoft.Xna.Framework.Graphics;
+using Terraria.ModLoader;
+
+namespace CalamityEntropy.Content.OtherMods.CalamityOverhaul
+{
+    //[JITWhenModsEnabled("CalamityOverhaul")]
+    //internal class TheIntimateLifeofTheHalibutAndTheNetherdrake : ADVScenarioBase, ILocalizedModType
+    //{
+    //    public override bool IsLoadingEnabled(Mod mod)
+    //    {
+    //        return ModLoader.HasMod("CalamityOverhaul");
+    //    }
+
+    //    protected override void Build()
+    //    {
+    //        if (ModLoader.HasMod("CalamityOverhaul")) {
+    //            DialogueBoxBase.RegisterPortrait("比目鱼", ADVAsset.HelenADV);
+    //            DialogueBoxBase.RegisterPortrait("始源妖龙", ModContent.Request<Texture2D>("CalamityEntropy/Content/NPCs/PrimordialWyrmNPC_Head").Value);
+    //            Add("比目鱼", "XXXXXXXXXXXXXXX");
+    //            Add("始源妖龙", "XXXXXXXXXXXXXXX");
+    //        }
+    //    }
+
+    //    public static void StartThis() {
+    //        if (ModLoader.HasMod("CalamityOverhaul")) {
+    //            ScenarioManager.Start<TheIntimateLifeofTheHalibutAndTheNetherdrake>();
+    //        }
+    //    }
+    //}
+}
