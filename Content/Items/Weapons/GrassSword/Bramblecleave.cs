69ff7dfd71a43bf828ee4515210fb74153372190
Materialistic989286067 <3608728857@qq.com>
2025-12-16 23:34:02 +0800

    草剑加强

--- Content/Items/Weapons/GrassSword/Bramblecleave.cs
+++ Content/Items/Weapons/GrassSword/Bramblecleave.cs
@@ -163,17 +163,17 @@ namespace CalamityEntropy.Content.Items.Weapons.GrassSword
             switch (level)
             {
                 case 0: dmg = 20; break;
-                case 1: dmg = 20; break;
+                case 1: dmg = 30; break;
                 case 2: dmg = 40; break;
                 case 3: dmg = 45; break;
                 case 4: dmg = 60; break;
                 case 5: dmg = 90; break;
                 case 6: dmg = 120; break;
                 case 7: dmg = 140; break;
-                case 8: dmg = 175; break;
+                case 8: dmg = 180; break;
                 case 9: dmg = 240; break;
                 case 10: dmg = 600; break;
-                case 11: dmg = 900; break;
+                case 11: dmg = 800; break;
                 case 12: dmg = 1300; break;
                 case 13: dmg = 1800; break;
                 case 14: dmg = 2200; break;
