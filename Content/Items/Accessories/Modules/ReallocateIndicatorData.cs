99c6bf48eaf97c8560abf556bf5b2cf4b30f79ce
Polaris <2058770793@qq.com>
2026-01-02 18:21:56 +0800

    调整

--- Content/Items/Accessories/Modules/ReallocateIndicatorData.cs
+++ Content/Items/Accessories/Modules/ReallocateIndicatorData.cs
@@ -38,15 +38,15 @@ namespace CalamityEntropy.Content.Items.Accessories.Modules
         public static void CalculateStatsForPlayer(Player player)
         {
             float MaxDeviation = 0.4f;
-            int seed = player.HeldItem.type * 2 + 7;
+            int seed = player.HeldItem.type + 14;
             if (Main.zenithWorld)
             {
                 seed += player.position.ToPoint().GetHashCode();
             }
-            float sum = new UnifiedRandom(seed).NextFloat(MaxDeviation / -4f, MaxDeviation / 2f);
+            float sum = new UnifiedRandom(seed).NextFloat(0, MaxDeviation / 2f);
             if (player.AzafureEnhance())
             {
-                sum += 0.3f;
+                sum += 0.38f;
             }
             List<float> ModifyMap = FloatListGenerator.GenerateFloatList(seed, 9, sum, -0.26f, 0.26f);
             float Dmg = ModifyMap[0];
