570ebbaa2d3092b60f4ab66c5258650e4ac79b42
Materialistic989286067 <3608728857@qq.com>
2026-03-08 23:54:41 +0800

    balance

--- Content/Items/VoidScales.cs
+++ Content/Items/VoidScales.cs
@@ -18,7 +18,7 @@ namespace CalamityEntropy.Content.Items
             Item.width = 16;
             Item.height = 16;
             Item.maxStack = 9999;
-            Item.value = Item.sellPrice(gold: 60);
+            Item.value = Item.sellPrice(gold: 45);
             Item.rare = ModContent.RarityType<VoidPurple>();
         }
     }
