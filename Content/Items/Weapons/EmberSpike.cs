1e9b7eab3958b0f71e833bf47585f6f9efcd6f27
Polaris <2058770793@qq.com>
2026-03-17 18:46:20 +0800

    盗贼武器

--- Content/Items/Weapons/EmberSpike.cs
+++ Content/Items/Weapons/EmberSpike.cs
@@ -19,9 +19,9 @@ using Terraria.ModLoader;
 
 namespace CalamityEntropy.Content.Items.Weapons
 {
-    public class CrystalSpike : RogueWeapon
+    public class EmberSpike : RogueWeapon
     {
-        public static int MAXSTICK => 8;
+        public static int MAXSTICK => 12;
         public override void ModifyTooltips(List<TooltipLine> tooltips)
         {
             tooltips.Replace("[S]", MAXSTICK.ToString());
@@ -30,7 +30,7 @@ namespace CalamityEntropy.Content.Items.Weapons
         {
             Item.width = 30;
             Item.height = 30;
-            Item.damage = 10;
+            Item.damage = 32;
             Item.noMelee = true;
             Item.noUseGraphic = true;
             Item.useAnimation = Item.useTime = 16;
@@ -41,16 +41,15 @@ namespace CalamityEntropy.Content.Items.Weapons
             Item.maxStack = 1;
             Item.value = CalamityGlobalItem.RarityBlueBuyPrice;
             Item.rare = ItemRarityID.Blue;
-            Item.shoot = ModContent.ProjectileType<CrystalSpikeThrow>();
+            Item.shoot = ModContent.ProjectileType<EmberSpikeThrow>();
             Item.shootSpeed = 12f;
             Item.DamageType = CEUtils.RogueDC;
         }
         public override void AddRecipes()
         {
             CreateRecipe()
-                .AddIngredient<UrchinStinger>()
-                .AddIngredient(ItemID.ManaCrystal, 2)
-                .AddTile(TileID.Anvils)
+                .AddIngredient<CrystalSpike>()
+                .AddIngredient<TectonicShard>(6)
                 .Register();
         }
         public override float StealthDamageMultiplier => 1;
@@ -76,29 +75,31 @@ namespace CalamityEntropy.Content.Items.Weapons
         }
         public void ReturnAllSpikes(Player player)
         {
-            int type = ModContent.ProjectileType<CrystalSpikeReturning>();
+            int type = ModContent.ProjectileType<EmberSpikeReturning>();
             int tm = 0;
             foreach(Projectile p in Main.ActiveProjectiles)
             {
-                if (p.owner == player.whoAmI && p.type == Item.shoot && p.ModProjectile is CrystalSpikeThrow cst && cst.StickNPC >= 0)
+                if (p.owner == player.whoAmI && p.type == Item.shoot && p.ModProjectile is EmberSpikeThrow cst && cst.StickNPC >= 0)
                 {
+                    
                     Projectile.NewProjectile(p.GetSource_FromThis(), p.Center, Vector2.Zero, type, p.damage, p.knockBack * 2, player.whoAmI, tm);
                     p.Kill();
                     tm += 2;
                 }
             }
-            if (tm > 0)
-                CEUtils.PlaySound("flashback", 1.6f, player.Center, 6, 0.55f);
+            if (tm > 0) 
+                CEUtils.PlaySound("RockCrumble", Main.rand.NextFloat(2.5f, 2.8f), player.Center, 60, 0.5f);
+
         }
     }
 
-    public class CrystalSpikeThrow : ModProjectile
+    public class EmberSpikeThrow : ModProjectile
     {
         public override void ModifyHitNPC(NPC target, ref NPC.HitModifiers modifiers)
         {
-            modifiers.ArmorPenetration += 12;
+            modifiers.ArmorPenetration += 16;
         }
-        public override string Texture => "CalamityEntropy/Content/Items/Weapons/CrystalSpike";
+        public override string Texture => "CalamityEntropy/Content/Items/Weapons/EmberSpike";
         public override void SetDefaults()
         {
             Projectile.FriendlySetDefaults(CEUtils.RogueDC, true, -1);
@@ -113,7 +114,7 @@ namespace CalamityEntropy.Content.Items.Weapons
         public override void AI()
         {
             if (counter == 0)
-                CEUtils.PlaySound((Projectile.ai[0] == 1 ? "crystalsound" : "bne") + (Main.rand.NextBool() ? "2" : "3"), Main.rand.NextFloat(1.4f, 1.6f), Projectile.Center, 6, 0.7f)
+                CEUtils.PlaySound("flamethrower end", Main.rand.NextFloat(4f, 4.2f), Projectile.Center, 6, 0.3f)
 ;            counter++;
             if (StickNPC == -1)
             {
@@ -124,13 +125,13 @@ namespace CalamityEntropy.Content.Items.Weapons
                     Projectile.velocity.X *= 0.98f;
                 }
                 if (Projectile.ai[0] == 0)
-                    EParticle.spawnNew(new CrystalGlow(), Projectile.Center, Vector2.Zero, Color.MediumPurple, 0.6f, 1, true, BlendState.Additive, 0, 20);
+                    EParticle.spawnNew(new GlowLightParticle(), Projectile.Center, Vector2.Zero, Color.OrangeRed, 0.6f, 1, true, BlendState.Additive, 0, 20);
                 else
-                    EParticle.spawnNew(new CrystalGlow(), Projectile.Center, Vector2.Zero, Color.MediumPurple, 0.36f, 1, true, BlendState.Additive, 0, 8);
+                    EParticle.spawnNew(new GlowLightParticle(), Projectile.Center, Vector2.Zero, Color.OrangeRed, 0.36f, 1, true, BlendState.Additive, 0, 8);
 
                 for (float i = 0; i < 1; i += 0.5f)
                     {
-                        var d = Dust.NewDustDirect(Projectile.Center, 0, 0, DustID.PurpleTorch);
+                        var d = Dust.NewDustDirect(Projectile.Center, 0, 0, DustID.RedTorch);
                         d.position = Vector2.Lerp(Projectile.Center - Projectile.velocity, Projectile.Center, i) + CEUtils.randomPointInCircle(5);
                         d.velocity = Projectile.velocity * Main.rand.NextFloat(0.4f);
                         d.noGravity = true;
@@ -146,6 +147,7 @@ namespace CalamityEntropy.Content.Items.Weapons
             if(StickNPC >= 0)
             {
                 Projectile.Center = StickNPC.ToNPC().Center + offset;
+                StickNPC.ToNPC().AddBuff(BuffID.OnFire3, 180);
             }
         }
         public override bool PreDraw(ref Color lightColor)
@@ -178,22 +180,36 @@ namespace CalamityEntropy.Content.Items.Weapons
         }
         public override void OnHitNPC(NPC target, NPC.HitInfo hit, int damageDone)
         {
-            EParticle.spawnNew(new CrystalGlow(), Projectile.Center + Projectile.rotation.ToRotationVector2() * 10, Vector2.Zero, Color.MediumPurple * 1.3f, 1.5f, 1, true, BlendState.Additive, 0, 10);
+            
+            EParticle.spawnNew(new GlowLightParticle(), Projectile.Center + Projectile.rotation.ToRotationVector2() * 10, Vector2.Zero, Color.OrangeRed * 1.3f, 1.5f, 1, true, BlendState.Additive, 0, 10);
 
-            CEUtils.PlaySound("truemoonlighthit", Main.rand.NextFloat(1.4f, 1.8f), target.Center, 60, 0.7f);
+            CEUtils.PlaySound("RockCrumble", Main.rand.NextFloat(2.4f, 2.8f), target.Center, 60, 0.4f);
             int sum = 0;
-            foreach(Projectile p in Main.ActiveProjectiles)
+            foreach(Projectile pj in Main.ActiveProjectiles)
             {
-                if (p.owner == Projectile.owner && p.type == Projectile.type && p.ModProjectile is CrystalSpikeThrow cs && cs.StickNPC == target.whoAmI)
+                if (pj.owner == Projectile.owner && pj.type == Projectile.type && pj.ModProjectile is EmberSpikeThrow cs && cs.StickNPC == target.whoAmI)
                     sum++;
             }
-            if (sum >= CrystalSpike.MAXSTICK) //Projectile.ai[0] == 1 || 
+            if (sum >= EmberSpike.MAXSTICK || Projectile.ai[0] == 1)
             {
-                Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, Projectile.velocity.RotatedByRandom(0.6f) * Main.rand.NextFloat(-1, -0.5f), ModContent.ProjectileType<CrystalSpikePop>(), 0, 0, Projectile.owner);
+                if(Projectile.Calamity().stealthStrike)
+                    Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, Projectile.velocity.RotatedByRandom(0.4f) * Main.rand.NextFloat(0.6f, 1f), ModContent.ProjectileType<TectinicShardHoming>(), Projectile.damage, 4, Projectile.owner);
+                Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, Projectile.velocity.RotatedByRandom(0.6f) * Main.rand.NextFloat(-1, -0.5f), ModContent.ProjectileType<EmberSpikePop>(), 0, 0, Projectile.owner);
                 Projectile.Kill();
             }
             else
             {
+                target.AddBuff(BuffID.OnFire3, 180);
+                Projectile p = Projectile;
+                Player player = Projectile.GetOwner();
+                CEUtils.SpawnExplotionFriendly(p.GetSource_FromThis(), player, p.Center, p.damage, 120, CEUtils.RogueDC);
+                float scale = 100 / 40f;
+                EParticle.spawnNew(new ShineParticle(), p.Center, Vector2.Zero, Color.OrangeRed * 0.8f, scale * 0.8f, 1, true, BlendState.Additive, 0, 10);
+                EParticle.spawnNew(new ShineParticle(), p.Center, Vector2.Zero, Color.Firebrick * 0.8f, scale * 0.5f, 1, true, BlendState.Additive, 0, 10);
+                GeneralParticleHandler.SpawnParticle(new CustomPulse(p.Center, Vector2.Zero, Color.OrangeRed * 1.4f, "CalamityMod/Particles/ShatteredExplosion", Vector2.One, CEUtils.randomRot(), 0.005f, scale * 0.05f, 24));
+                GeneralParticleHandler.SpawnParticle(new CustomPulse(p.Center, Vector2.Zero, Color.OrangeRed * 1.4f, "CalamityMod/Particles/ShatteredExplosion", Vector2.One, CEUtils.randomRot(), 0.005f, scale * 0.035f, 18));
+                GeneralParticleHandler.SpawnParticle(new CustomPulse(p.Center, Vector2.Zero, Color.OrangeRed * 1.4f, "CalamityMod/Particles/ShatteredExplosion", Vector2.One, CEUtils.randomRot(), 0.005f, scale * 0.02f, 15));
+
                 if (StickNPC < 0)
                 {
                     StickNPC = target.whoAmI;
@@ -205,17 +221,17 @@ namespace CalamityEntropy.Content.Items.Weapons
         }
         public override bool OnTileCollide(Vector2 oldVelocity)
         {
-            EParticle.spawnNew(new CrystalGlow(), Projectile.Center + oldVelocity + Projectile.rotation.ToRotationVector2() * 4, Vector2.Zero, Color.MediumPurple * 1.3f, 1.5f, 1, true, BlendState.Additive, 0, 10);
+            EParticle.spawnNew(new GlowLightParticle(), Projectile.Center + oldVelocity + Projectile.rotation.ToRotationVector2() * 4, Vector2.Zero, Color.OrangeRed * 1.3f, 1.5f, 1, true, BlendState.Additive, 0, 10);
             if (Main.myPlayer == Projectile.owner)
             {
-                Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, oldVelocity.RotatedByRandom(0.6f) * Main.rand.NextFloat(-1, -0.5f), ModContent.ProjectileType<CrystalSpikePop>(), 0, 0, Projectile.owner);
+                Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.Center, oldVelocity.RotatedByRandom(0.6f) * Main.rand.NextFloat(-1, -0.5f), ModContent.ProjectileType<EmberSpikePop>(), 0, 0, Projectile.owner);
             }
             return true;
         }
     }
-    public class CrystalSpikePop : ModProjectile
+    public class EmberSpikePop : ModProjectile
     {
-        public override string Texture => "CalamityEntropy/Content/Items/Weapons/CrystalSpike";
+        public override string Texture => "CalamityEntropy/Content/Items/Weapons/EmberSpike";
         public override void SetDefaults()
         {
             Projectile.FriendlySetDefaults(CEUtils.RogueDC, false, -1);
@@ -240,9 +256,9 @@ namespace CalamityEntropy.Content.Items.Weapons
             return false;
         }
     }
-    public class CrystalSpikeReturning : ModProjectile
+    public class EmberSpikeReturning : ModProjectile
     {
-        public override string Texture => "CalamityEntropy/Content/Items/Weapons/CrystalSpike";
+        public override string Texture => "CalamityEntropy/Content/Items/Weapons/EmberSpike";
         public override void SetDefaults()
         {
             Projectile.FriendlySetDefaults(CEUtils.RogueDC, false, -1);
@@ -260,16 +276,16 @@ namespace CalamityEntropy.Content.Items.Weapons
             counter++;
             Vector2 mid = (spawn + target) * 0.5f + ofst;
             if (counter == 36 && Projectile.ai[0] == 0 && Main.myPlayer == Projectile.owner)
-                Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.GetOwner().Center, Vector2.Zero, ModContent.ProjectileType<SwingSmear>(), 0, 0, Projectile.owner);
+                Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.GetOwner().Center, Vector2.Zero, ModContent.ProjectileType<SwingSmearEmber>(), 0, 0, Projectile.owner);
             if (counter <= 36f)
             {
                 Vector2 pos = CEUtils.Bezier(new List<Vector2>() { spawn, mid, target }, counter / 36f);
                 Vector2 offset = pos - Projectile.Center;
                 Projectile.rotation += offset.X * 0.05f;
-                EParticle.spawnNew(new CrystalGlow(), Projectile.Center, Vector2.Zero, Color.MediumPurple, 0.6f, 1, true, BlendState.Additive, 0, 20);
+                EParticle.spawnNew(new GlowLightParticle(), Projectile.Center, Vector2.Zero, Color.OrangeRed, 0.6f, 1, true, BlendState.Additive, 0, 20);
                 for (float i = 0; i < 1; i += 0.5f)
                 {
-                    var d = Dust.NewDustDirect(Projectile.Center, 0, 0, DustID.PurpleTorch);
+                    var d = Dust.NewDustDirect(Projectile.Center, 0, 0, DustID.RedTorch);
                     d.position = Vector2.Lerp(Projectile.Center, pos, i) + CEUtils.randomPointInCircle(5);
                     d.velocity = offset * Main.rand.NextFloat(0.4f);
                     d.noGravity = true;
@@ -285,7 +301,7 @@ namespace CalamityEntropy.Content.Items.Weapons
                     if (Main.myPlayer == Projectile.owner)
                     {
                         Vector2 vel = (Main.MouseWorld - Projectile.GetOwner().MountedCenter).normalize() * 15;
-                        int p = Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.GetOwner().MountedCenter + vel.normalize().RotatedBy(MathHelper.PiOver2) * Main.rand.NextFloat(-10, 10), vel, ModContent.ProjectileType<CrystalSpikeThrow>(), Projectile.damage, Projectile.knockBack, Projectile.owner, 1);
+                        int p = Projectile.NewProjectile(Projectile.GetSource_FromThis(), Projectile.GetOwner().MountedCenter + vel.normalize().RotatedBy(MathHelper.PiOver2) * Main.rand.NextFloat(-10, 10), vel, ModContent.ProjectileType<EmberSpikeThrow>(), Projectile.damage, Projectile.knockBack, Projectile.owner, 1);
                         p.ToProj().Calamity().stealthStrike = true;
                         CEUtils.SyncProj(p);
                     }
@@ -305,7 +321,7 @@ namespace CalamityEntropy.Content.Items.Weapons
             return false;
         }
     }
-    public class SwingSmear: ModProjectile
+    public class SwingSmearEmber: ModProjectile
     {
         public override void SetDefaults()
         {
@@ -316,13 +332,9 @@ namespace CalamityEntropy.Content.Items.Weapons
         {
             return false;
         }
-        public override void DrawBehind(int index, List<int> behindNPCsAndTiles, List<int> behindNPCs, List<int> behindProjectiles, List<int> overPlayers, List<int> overWiresUI)
-        {
-            overPlayers.Add(index);
-        }
         public override void AI()
         {
-            if (Projectile.GetOwner().ownedProjectileCounts[ModContent.ProjectileType<CrystalSpikeReturning>()] > 0)
+            if (Projectile.GetOwner().ownedProjectileCounts[ModContent.ProjectileType<EmberSpikeReturning>()] > 0)
                 Projectile.timeLeft = 10;
             Projectile.Center = Projectile.GetOwner().GetDrawCenter();
             Projectile.rotation += 0.66f;
@@ -330,10 +342,14 @@ namespace CalamityEntropy.Content.Items.Weapons
                 Main.LocalPlayer.direction = Main.MouseWorld.X > Main.LocalPlayer.Center.X ? 1 : -1;
             Projectile.GetOwner().SetHandRotWithDir(Projectile.rotation * Projectile.GetOwner().direction + (Projectile.GetOwner().direction > 0 ? 0 : MathHelper.Pi) + Projectile.GetOwner().direction * -0.6f, Projectile.GetOwner().direction);
         }
+        public override void DrawBehind(int index, List<int> behindNPCsAndTiles, List<int> behindNPCs, List<int> behindProjectiles, List<int> overPlayers, List<int> overWiresUI)
+        {
+            overPlayers.Add(index);
+        }
         public override bool PreDraw(ref Color lightColor)
         {
             Main.spriteBatch.UseBlendState(BlendState.Additive);
-            Main.spriteBatch.Draw(Projectile.GetTexture(), Projectile.Center - Main.screenPosition, null, Color.MediumPurple * 1.25f * (Projectile.timeLeft / 10f), Projectile.rotation * Projectile.GetOwner().direction, Projectile.GetTexture().Size() * 0.5f, Projectile.scale * 0.36f, Projectile.GetOwner().direction > 0 ? SpriteEffects.None : SpriteEffects.FlipHorizontally, 0);
+            Main.spriteBatch.Draw(Projectile.GetTexture(), Projectile.Center - Main.screenPosition, null, Color.OrangeRed * 1.25f * (Projectile.timeLeft / 10f), Projectile.rotation * Projectile.GetOwner().direction, Projectile.GetTexture().Size() * 0.5f, Projectile.scale * 0.36f, Projectile.GetOwner().direction > 0 ? SpriteEffects.None : SpriteEffects.FlipHorizontally, 0);
             Main.spriteBatch.ExitShaderRegion();
             return false;
         }
