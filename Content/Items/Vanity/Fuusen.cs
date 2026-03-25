6a9211801d1ac729472872b0cbc186ba5297d54f
Polaris <2058770793@qq.com>
2026-03-26 02:44:38 +0800

    -

---
+++ Content/Items/Vanity/Fuusen.cs
@@ -0,0 +1,66 @@
+﻿using CalamityEntropy.Common;
+using CalamityEntropy.Content.Items.Donator;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace CalamityEntropy.Content.Items.Vanity
+{
+    public class Fuusen : ModItem, IDonatorItem, IVanitySkin
+    {
+        public string DonatorName => "脸f";
+
+        public override void Load()
+        {
+            if (Main.netMode != NetmodeID.Server)
+            {
+                EquipLoader.AddEquipTexture(Mod, $"{Mod.Name.ToString()}/Content/Items/Vanity/{Name}_Head", EquipType.Head, this);
+                EquipLoader.AddEquipTexture(Mod, $"{Mod.Name.ToString()}/Content/Items/Vanity/{Name}_Body", EquipType.Body, this);
+                EquipLoader.AddEquipTexture(Mod, $"{Mod.Name.ToString()}/Content/Items/Vanity/{Name}_Legs", EquipType.Legs, this);
+            }
+        }
+
+        public override void SetStaticDefaults()
+        {
+            if (Main.netMode == NetmodeID.Server)
+                return;
+
+            int equipSlotHead = EquipLoader.GetEquipSlot(Mod, Name, EquipType.Head);
+            ArmorIDs.Head.Sets.DrawHead[equipSlotHead] = false;
+
+            int equipSlotBody = EquipLoader.GetEquipSlot(Mod, Name, EquipType.Body);
+            ArmorIDs.Body.Sets.HidesTopSkin[equipSlotBody] = true;
+            ArmorIDs.Body.Sets.HidesArms[equipSlotBody] = true;
+
+            int equipSlotLegs = EquipLoader.GetEquipSlot(Mod, Name, EquipType.Legs);
+            ArmorIDs.Legs.Sets.HidesBottomSkin[equipSlotLegs] = true;
+        }
+
+        public override void SetDefaults()
+        {
+            Item.width = 32;
+            Item.height = 32;
+            Item.accessory = true;
+            Item.vanity = true;
+            Item.value = Item.buyPrice(0, 1, 0, 0);
+            Item.rare = ItemRarityID.Yellow;
+        }
+
+        public override void UpdateVanity(Player player)
+        {
+            player.GetModPlayer<VanityModPlayer>().vanityEquipped = Name;
+        }
+
+        public override void UpdateAccessory(Player player, bool hideVisual)
+        {
+            if (!hideVisual)
+            {
+                player.GetModPlayer<VanityModPlayer>().vanityEquipped = Name;
+            }
+        }
+
+        public override void AddRecipes()
+        {
+        }
+    }
+}
