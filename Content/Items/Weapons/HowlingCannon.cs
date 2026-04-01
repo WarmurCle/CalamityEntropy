760530f1fd1f2d61cf4c5a8a1579bdc138e7ca81
Polaris <2058770793@qq.com>
2026-04-01 17:13:42 +0800

    0

--- Content/Items/Weapons/HowlingCannon.cs
+++ Content/Items/Weapons/HowlingCannon.cs
@@ -94,6 +94,11 @@ namespace CalamityEntropy.Content.Items.Weapons
         public override void AI()
         {
             Player player = Projectile.GetOwner();
+            if(player.dead)
+            {
+                Projectile.Kill();
+                return;
+            }
             if (player.HeldItem.ModItem is HowlingCannon hc)
             {
                 Projectile.timeLeft = 2;
