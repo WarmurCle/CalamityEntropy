0a167f55a4de610e97fcd608b33c11799c552b59
Materialistic989286067 <3608728857@qq.com>
2026-04-04 11:43:24 +0800

    balance

--- Content/Items/Weapons/Whips/ForeseeWhip.cs
+++ Content/Items/Weapons/Whips/ForeseeWhip.cs
@@ -8,12 +8,12 @@ namespace CalamityEntropy.Content.Items.Weapons.Whips
 {
     public class ForeseeWhip : BaseWhipItem
     {
-        public override int TagDamage => 8;
-        public override float TagDamageMult => 1.12f;
-        public override float TagCritChance => 0.16f;
+        public override int TagDamage => 10;
+        public override float TagDamageMult => 1.05f;
+        public override float TagCritChance => 0.10f;
         public override void SetDefaults()
         {
-            Item.DefaultToWhip(ModContent.ProjectileType<ForeseeWhipProj>(), 80, 3, 4, 24);
+            Item.DefaultToWhip(ModContent.ProjectileType<ForeseeWhipProj>(), 100, 3, 4, 24);
             Item.rare = ItemRarityID.Yellow;
             Item.value = CalamityGlobalItem.RarityYellowBuyPrice;
             Item.autoReuse = true;
