6a96f8f7732354f84f7bd17ed75d9defa51e1c64
Polaris <2058770793@qq.com>
2026-05-09 17:06:05 +0800

    Placable giant hive wall

---
+++ Content/Items/Walls/BeewaxWall.cs
@@ -0,0 +1,26 @@
+﻿using CalamityMod.Items.Placeables.FurnitureAcidwood;
+using CalamityMod.Walls;
+using Terraria.ID;
+using Terraria.ModLoader;
+using WallTiles = CalamityMod.Walls;
+
+namespace CalamityEntropy.Content.Items.Walls
+{
+    public class BeewaxWall : ModItem
+    {
+        public override void SetStaticDefaults()
+        {
+            Item.ResearchUnlockCount = 200;
+        }
+
+        public override void SetDefaults() => Item.DefaultToPlaceableWall(ModContent.WallType<GiantHiveWall>());
+
+        public override void AddRecipes()
+        {
+            CreateRecipe(40).
+            AddIngredient(ItemID.BeeWax).
+            AddTile(TileID.HoneyDispenser).
+            Register();
+        }
+    }
+}
