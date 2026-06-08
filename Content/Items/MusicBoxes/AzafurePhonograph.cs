283476341b349e44dd1ce4b92b7d9e6c4200d8b5
Polaris <2058770793@qq.com>
2026-06-08 08:47:53 +0800

    -

--- Content/Items/MusicBoxes/AzafurePhonograph.cs
+++ Content/Items/MusicBoxes/AzafurePhonograph.cs
@@ -20,7 +20,7 @@ namespace CalamityEntropy.Content.Items.MusicBoxes
             if (t.TileFrameX % 36 == 0 && t.TileFrameY % 36 == 0)
             {
                 Vector2 scaling = new Vector2(1 + (float)Math.Sin(Main.GameUpdateCount * 0.06f) * 0.05f, 1 + (float)Math.Sin(Main.GameUpdateCount * 0.06f + MathHelper.Pi) * 0.05f);
-                Rectangle rect = t.TileFrameX == 0 ? new Rectangle(0, 0, 56, 46) : new Rectangle(60, 0, 60, 46);
+                Rectangle rect = t.TileFrameX == 0 ? new Rectangle(0, 0, 60, 46) : new Rectangle(60, 0, 60, 46);
                 Main.spriteBatch.Draw(tex, new Vector2(i * 16 + 16 * 13, j * 16 + 16 * 14) - Main.screenPosition, rect, Lighting.GetColor(new Point(i, j)), 0, new Vector2(30, 46), t.TileFrameX == 0 ? Vector2.One : scaling, SpriteEffects.None, 0);
             }
             return false;
