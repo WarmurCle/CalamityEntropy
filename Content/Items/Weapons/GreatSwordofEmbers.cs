76ebac8e34b313ba39b9022bafa4b558496c6e35
Materialistic989286067 <3608728857@qq.com>
2026-06-20 01:44:44 +0800

    真近战

--- Content/Items/Weapons/GreatSwordofEmbers.cs
+++ Content/Items/Weapons/GreatSwordofEmbers.cs
@@ -21,7 +21,7 @@ namespace CalamityEntropy.Content.Items.Weapons
         public int ProjectileType => Item.shoot;
         public override void SetDefaults()
         {
-            Item.damage = 142;
+            Item.damage = 150;
             Item.DamageType = ModContent.GetInstance<TrueMeleeDamageClass>();
             Item.width = 70;
             Item.height = 72;
