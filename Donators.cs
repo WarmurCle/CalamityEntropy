f3d3016a10330bf97ba746a0abfbc2f4be1feb01
Polaris <2058770793@qq.com>
2026-07-18 21:14:14 +0800

    0

--- Donators.cs
+++ Donators.cs
@@ -7,12 +7,13 @@ namespace CalamityEntropy
         public static List<string> Donors = new List<string>()
         {
             "冷级", "冻结", "尘Dust", "烟玉", "心斩狂歌", "玲瓏", "ChaLost", "银九", "Kernschmelze",
-            "Sora", "momodzmz", "锯角", "Away", "梦龍", "Badbf-HX", "Yharim", "Hocha", "Kikastorm", "Leon金蜂鸟",
+            "Sora", "momodzmz", "锯角", "Away", "梦龍", "Badbf-HX",
             "Wuti-v-", "AtmospherePredator", "松饼", "a3a4", "Zenith", "球球疑惑", "余myku", "The Fool",
             "Votum_Stellarum", "Nienie", "Carter", "Reficul", "OVASA", "Shadow Warrior", "ScarletShelf",
-            "黯月殇梦", "鹰身吹雪", "Rathyep", "新星之眼", "嘉登糖", "Carter", "繁星与你共游", "Kino", "血清素",
+            "黯月殇梦", "鹰身吹雪", "Rathyep", "新星之眼", "Carter", "繁星与你共游", "Kino", "血清素",
             "D_Addict", "散尘化天心", "紊䒥", "Armonia", "薄良荷影", "九月", "周珛", "凛栖", "五彩斑斓的黑",
-            "鈴夕", "薰依", "脸F", "Fortun3Rod1on", "Dannon", "Soba Noodles", "Austin Moore"
+            "鈴夕", "薰依", "脸F", "Fortun3Rod1on", "Dannon", "Soba Noodles", "Austin Moore",
+            "普维莉雅"
         };
     }
 }
