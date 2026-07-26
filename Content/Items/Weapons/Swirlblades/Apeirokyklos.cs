a5e864847babe8f211242877025373d720b26232
Polaris <2058770793@qq.com>
2026-07-26 13:23:13 +0800

    -

--- Content/Items/Weapons/Swirlblades/Apeirokyklos.cs
+++ Content/Items/Weapons/Swirlblades/Apeirokyklos.cs
@@ -41,7 +41,7 @@ namespace CalamityEntropy.Content.Items.Weapons.Swirlblades
             Item.noMelee = true;
             Item.noUseGraphic = true;
         }
-        public override float StealthDamageMultiplier => 1.2f;
+        public override float StealthDamageMultiplier => 1.6f;
         public override float StealthVelocityMultiplier => 1.2f;
 
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
