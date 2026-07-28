962ed8874ed2af55e0a3ccdf873cf3dc3249471b
Materialistic989286067 <3608728857@qq.com>
2026-07-28 11:33:13 +0800

    旋刃
    Reduced StealthDamageMultiplier from 0.6 to 0.5.

--- Content/Items/Weapons/Swirlblades/SawofMacrocosm.cs
+++ Content/Items/Weapons/Swirlblades/SawofMacrocosm.cs
@@ -50,7 +50,7 @@ namespace CalamityEntropy.Content.Items.Weapons.Swirlblades
             Item.noMelee = true;
             Item.noUseGraphic = true;
         }
-        public override float StealthDamageMultiplier => 0.6f;
+        public override float StealthDamageMultiplier => 0.5f;
         public override float StealthVelocityMultiplier => 1.25f;
 
         public override bool Shoot(Player player, EntitySource_ItemUse_WithAmmo source, Vector2 position, Vector2 velocity, int type, int damage, float knockback)
